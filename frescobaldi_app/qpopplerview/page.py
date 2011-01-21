00d2bd744b739c559d05d81d8a96315ff27dacd4
wbsoft <wbsoft@xs4all.nl>
2011-01-21 19:46:43 +0000

    more page logic, todo: add scale factor

--- frescobaldi_app/qpopplerview/page.py
+++ frescobaldi_app/qpopplerview/page.py
@@ -5,6 +5,8 @@ A Page is resposible for drawing a page of a Poppler document
 inside a layout.
 """
 
+import popplerqt4
+
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
@@ -14,47 +16,99 @@ class Page(object):
         self._document = document
         self._pageNumber = pageNumber
         self._pageSizeF = document.page(pageNumber).pageSizeF()
+        self._rotation = popplerqt4.Poppler.Page.Rotate0
         self._rect = QRect()
+        self.setDPI(72.0)
         self._layout = lambda: None
         
     def document(self):
+        """Returns the document."""
         return self._document
         
     def pageNumber(self):
+        """Returns the page number."""
         return self._pageNumber
         
     def layout(self):
+        """Returns the Layout if we are part of one."""
         return self._layout()
     
-    def setLayout(self, layout):
-        if self not in layout:
-            layout.addPage(self)
-            
     def rect(self):
+        """Returns our QRect(), with position and size."""
         return self._rect
     
     def size(self):
+        """Returns our size."""
         return self._rect.size()
     
-    def setSize(self, size):
-        self._rect.setSize(size)
+    def height(self):
+        """Returns our height."""
+        return self._rect.height()
+        
+    def width(self):
+        """Returns our width."""
+        return self._rect.width()
         
     def pos(self):
+        """Returns our position."""
         return self._rect.topLeft()
     
     def setPos(self, point):
+        """Sets our position (affects the Layout)."""
         self._rect.setTopLeft(point)
     
     def setDPI(self, xres, yres=None):
+        """Sets our dots per inch in x and y direction.
+        
+        If y is not given, the same value as for x is used.
+        
+        """
         if yres is None:
             yres = xres
-        x = int(round(self._pageSizeF.width() * xres / 72.0))
-        y = int(round(self._pageSizeF.height() * yres / 72.0))
-        self.setSize(QSize(x, y))
+        self._xres = xres
+        self._yres = yres
+        
+        s = self._pageSizeF
+        if self._rotation & 1:
+            s.transpose()
+        x = int(round(s.width() * xres / 72.0))
+        y = int(round(s.height() * yres / 72.0))
+        self._rect.setSize(QSize(x, y))
+    
+    def dpi(self):
+        """Returns a tuple (xres, yres)."""
+        return self._xres, self._yres
+        
+    def setRotation(self, rotation):
+        """Sets our Poppler.Page.Rotation."""
+        old, self._rotation = self._rotation, rotation
+        if (old ^ rotation) & 1:
+            # swap x and y in our size
+            s = self._rect.size()
+            s.transpose()
+            self._rect.setSize(s)
+    
+    def rotation(self):
+        """Returns our rotation."""
+        return self._rotation
+    
+    def setWidth(self, width):
+        """Forces our width (influences size() and dpi())."""
+        if width != self.width():
+            scale = float(width) / self._rect.width()
+            xres, yres = self.dpi()
+            self.setDPI(xres * scale, yres * scale)
+
+    def setHeight(self, height):
+        """Forces our height (influences size() and dpi())."""
+        if height != self.height():
+            scale = float(height) / self._rect.height()
+            xres, yres = self.dpi()
+            self.setDPI(xres * scale, yres * scale)
         
     def paint(self, painter, rect):
         update_rect = rect & self.rect()
-        if not update_rect.isValid():
+        if not update_rect:
             return
         image = cache.image(self._document, self._pageNumber, self.size())
         if image:
