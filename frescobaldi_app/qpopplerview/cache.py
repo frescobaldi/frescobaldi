a979caa90e76a423f2e9e95c27ed6a1a1858de7f
wbsoft <wbsoft@xs4all.nl>
2011-01-21 15:34:42 +0000

    cache stub

---
+++ frescobaldi_app/qpopplerview/cache.py
@@ -0,0 +1,30 @@
+#! python
+
+"""
+A Page is resposible for drawing a page of a Poppler document
+inside a layout.
+"""
+
+import weakref
+import popplerqt4
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+_cache = weakref.WeakKeyDictionary()
+
+
+def image(document, pageNumber, size, rotation=popplerqt4.Poppler.Page.Rotate0):
+    """Returns a rendered image in size."""
+    key = (pageNumber, size.width(), size.height(), rotation)
+    try:
+        return _cache[document][key]
+    except KeyError:
+        page = document.page(pageNumber)
+        xres = 72.0 * size.width() / page.pageSizeF().width()
+        yres = 72.0 * size.height() / page.pageSizeF().height()
+        image = page.renderToImage(xres, yres, -1, -1, -1, -1, rotation)
+        _cache.setdefault(document, {})[key] = image
+        return image
+
+
--- frescobaldi_app/qpopplerview/page.py
+++ frescobaldi_app/qpopplerview/page.py
@@ -11,11 +11,11 @@ from PyQt4.QtGui import *
 
 class Page(object):
     def __init__(self, document, pageNumber):
-        self._pageNumber = pageNumber
         self._document = document
-        self._page = document.page(pageNumber)
-        
+        self._pageNumber = pageNumber
+        self._pageSizeF = document.page(pageNumber).pageSizeF()
         self._rect = QRect()
+        self._layout = lambda: None
         
     def document(self):
         return self._document
@@ -23,15 +23,45 @@ class Page(object):
     def pageNumber(self):
         return self._pageNumber
         
+    def layout(self):
+        return self._layout()
+    
+    def setLayout(self, layout):
+        if self not in layout:
+            layout.addPage(self)
+            
     def rect(self):
         return self._rect
+    
+    def size(self):
+        return self._rect.size()
+    
+    def setSize(self, size):
+        self._rect.setSize(size)
+        
+    def pos(self):
+        return self._rect.topLeft()
+    
+    def setPos(self, point):
+        self._rect.setTopLeft(point)
+    
+    def setDPI(self, xres, yres=None):
+        if yres is None:
+            yres = xres
+        x = int(round(self._pageSizeF.width() * xres / 72.0))
+        y = int(round(self._pageSizeF.height() * yres / 72.0))
+        self.setSize(QSize(x, y))
         
     def paint(self, painter, rect):
         update_rect = rect & self.rect()
         if not update_rect.isValid():
             return
-        image = cache.image(self._document, self._rect.size())
+        image = cache.image(self._document, self._pageNumber, self.size())
         if image:
             image_rect = QRect(update_rect.topLeft() - self.rect().topLeft(), ur.size())
-        
+            painter.drawImage(update_rect, image, image_rect)
+        else:
+            painter.fillRect(update_rect, QApplication.palette().background().color())
+
+
         
