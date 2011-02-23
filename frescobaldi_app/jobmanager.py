0d289a93b1d10dde68eedaecda44e0d1158c6f26
Wilbert Berendsen <wbsoft@xs4all.nl>
2011-02-23 12:53:20 +0100

    start job stuff

---
+++ frescobaldi_app/jobmanager.py
@@ -0,0 +1,58 @@
+# This file is part of the Frescobaldi project, http://www.frescobaldi.org/
+#
+# Copyright (c) 2008, 2009, 2010 by Wilbert Berendsen
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# See http://www.gnu.org/licenses/ for more information.
+
+from __future__ import unicode_literals
+
+"""
+A JobManager exists for every Document, and ensures no two jobs are running
+at the same time.
+
+It also sends the app-wide signals jobStarted() and jobFinished().
+
+"""
+
+import app
+import plugin
+
+
+def manager(document):
+    return JobManager.instance(document)
+
+
+class JobManager(plugin.DocumentPlugin):
+    def __init__(self, document):
+        self._running = None
+        
+    def startJob(self, job):
+        """Starts a Job on our behalf."""
+        if not self._running:
+            self._running = job
+            job.done.connect(self._finished)
+            job.start()
+            app.jobStarted(self.document(), job)
+        
+    def _finished(self, success):
+        app.jobFinished(self.document(), self._running, success)
+        self._running = None
+    
+    def runningJob(self):
+        return self._running
+
+
+
