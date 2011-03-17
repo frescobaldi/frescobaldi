6c28cd5f8a9061ac94f4adc709d63e356dacfc86
Wilbert Berendsen <wbsoft@xs4all.nl>
2011-03-17 23:55:19 +0100

    documentgroup starts getting into shape

--- frescobaldi_app/musicview/document.py
+++ frescobaldi_app/musicview/document.py
@@ -35,6 +35,7 @@ import popplerqt4
 import app
 import plugin
 import resultfiles
+import signals
 
 
 _cache = weakref.WeakValueDictionary()
@@ -93,29 +94,66 @@ class DocumentGroup(plugin.DocumentPlugin):
     One of the documents is the current document, displayed in the viewer.
     
     """
+    
+    # reloaded() is emitted when the documents have been updated,
+    # the current index may then also have been changed.
+    reloaded = signals.Signal()
+    
+    # emitted when the current document is changed via setCurrentIndex()
+    currentIndexChanged = signals.Signal() # int
+    
     def __init__(self, document):
-        self._docs = []
+        self._documents = []
         self._current = 0
-        self._dirty = True
         
     def documents(self):
-        """Returns the list of PDF Document objects created by our text document."""
-        self.update()
-        return self._docs[:]
-            
+        """Returns the list of PDF Document objects created by our text document.
+        
+        update() must have been called before calling this.
+        
+        """
+        return self._documents[:]
+    
     def update(self):
-        if self._dirty:
+        """Queries the resultfiles of this text document for PDF files and loads them.
+        
+        emits the reloaded() signal if there were any documents.
+        
+        """
+        files = resultfiles.results(self.document()).files(".pdf")
+        if files:
+            # reuse the older Document objects, they will probably be displaying
+            # (about) the same documents, and so they will retain their position
+            # in the viewer.
             def docs():
-                """Yields existing docs and then new ones."""
-                for d in self._docs:
+                # yield existing docs and then new ones
+                for d in self._documents:
                     yield d
                 while True:
                     yield Document()
-            docs = []
-            files = resultfiles.results(self.document()).files(".pdf")
+            documents = []
             for filename, doc in zip(files, docs()):
                 doc.setFilename(filename)
-                docs.append(docs)
-            self._docs = docs
-            self._dirty = False
+                documents.append(doc)
+            self._documents = documents
+            if self._current >= len(files):
+                self._current = 0
+            self.reloaded()
+
+    def setCurrentIndex(self, index):
+        """Changes the current document and emits the currentIndexChanged(int) signal."""
+        if index < len(self._documents) and index != self._current:
+            self._current = index
+            self.currentIndexChanged(index)
+    
+    def currentIndex(self):
+        """Returns the index of the current document in the list returned by documents()."""
+        return self._current
+
+
+
+@app.jobFinished.connect
+def _update_documents(document):
+    DocumentGroup.instance(document).update()
+
 
