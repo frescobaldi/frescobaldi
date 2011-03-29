e2780e7451d858d2b1991a18fe63a8c9d214323d
Wilbert Berendsen <wbsoft@xs4all.nl>
2011-03-29 11:52:15 +0200

    script to create POT file, document the translation process

---
+++ update-pot.py
@@ -0,0 +1,32 @@
+#! python
+
+# This small scripts creates a POT file for the translations by extracting all messages
+# from Python source files.
+
+import os
+import subprocess
+import sys
+
+from frescobaldi_app import info
+
+command = [
+    'xgettext',
+    '--language=python',
+    '--output={0}.pot'.format(info.name),
+    '--package-name={0}'.format(info.name),
+    '--package-version={0}'.format(info.version),
+    '--msgid-bugs-address={0}'.format(info.maintainer_email),
+    '--keyword',                # empty the default keyword list
+    '--keyword=_:1c,2,3,4t',    # context, message, plural, count
+    '--keyword=_:1,2,3t',       # message, plural, count
+    '--keyword=_:1c,2,2t',      # context, message
+    '--keyword=_:1,1t',         # message
+    '--add-comments=L10N',
+]
+
+for root, dirs, files in os.walk('frescobaldi_app'):
+    for f in files:
+        if f.endswith('.py') and f[0] != '.':
+            command.append(os.path.join(root, f))
+
+sys.exit(subprocess.call(command))
