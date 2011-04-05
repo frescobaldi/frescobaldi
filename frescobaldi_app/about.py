9f5eae24898487702883d3a8c638c7dc794dc8d4
Wilbert Berendsen <wbsoft@xs4all.nl>
2011-04-05 18:17:14 +0200

    mention translator and smarter way to get the pixmap

--- frescobaldi_app/about.py
+++ frescobaldi_app/about.py
@@ -23,13 +23,12 @@ from __future__ import unicode_literals
 About dialog.
 """
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
-from PyQt4.QtSvg import *
+from PyQt4.QtCore import QSize, Qt
+from PyQt4.QtGui import QDialog, QDialogButtonBox, QLabel, QLayout, QVBoxLayout
 
 import app
 import info
-
+import icons
 
 
 class AboutDialog(QDialog):
@@ -40,49 +39,14 @@ class AboutDialog(QDialog):
         layout = QVBoxLayout()
         self.setLayout(layout)
         
-        # large icon size
         size = QSize(100, 100)
-        
-        # draw SVG
-        r = QSvgRenderer("icons:frescobaldi.svg")
-        pixmap = QPixmap(size)
-        pixmap.fill(Qt.transparent)
-        r.render(QPainter(pixmap))
-        
         pic = QLabel()
-        pic.setPixmap(pixmap)
+        pic.setPixmap(icons.get("frescobaldi").pixmap(size))
         pic.setFixedSize(size)
-        
         layout.addWidget(pic, 0, Qt.AlignHCenter)
 
-        # Html
-        html = """<html>
-        <body><div align="center">
-        <h1>{appname}</h1>
-        <h3>{version}</h3>
-        <p>{description}</p>
-        <p>{copyright}</p>
-        <p>{license}</p>
-        <p><a href="{homepage}">{homepage}</a></p>
-        </div></body>
-        </html>
-        """.format(
-            appname = info.appname,
-            version = _("Version {version}").format(version = info.version),
-            description = _("A LilyPond Music Editor"),
-            copyright = _("Copyright (c) {year} by {author}").format(
-                year = "2008-2011",
-                author = """<a href="mailto:{0}" title="{1}">{2}</a>""".format(
-                    info.maintainer_email,
-                    _("Send an e-mail message to the maintainers."),
-                    info.maintainer)),
-            license = _("Licensed under the {gpl}").format(
-                gpl = """<a href="http://www.gnu.org/licenses/gpl.html">GNU GPL</a>"""),
-            homepage = info.url,
-        )
-        
         text = QLabel()
-        text.setText(html)
+        text.setText(html())
         text.setOpenExternalLinks(True)
         layout.addWidget(text)
 
@@ -93,3 +57,38 @@ class AboutDialog(QDialog):
         layout.setSizeConstraint(QLayout.SetFixedSize)
 
 
+def html():
+    """Returns a HTML string for the about dialog."""
+    appname = info.appname
+    version = _("Version {version}").format(version = info.version)
+    description = _("A LilyPond Music Editor")
+    copyright = _("Copyright (c) {year} by {author}").format(
+        year = "2008-2011",
+        author = """<a href="mailto:{0}" title="{1}">{2}</a>""".format(
+            info.maintainer_email,
+            _("Send an e-mail message to the maintainers."),
+            info.maintainer))
+    # L10N: Translate this sentence and fill in your own name to have it appear in the About Dialog.
+    translator = _("Translated by Your Name.")
+    if translator == "Translated by Your Name.":
+        translator = ""
+    else:
+        translator = "<p>{0}</p>".format(translator)
+    license = _("Licensed under the {gpl}.").format(
+        gpl = """<a href="http://www.gnu.org/licenses/gpl.html">GNU GPL</a>""")
+    homepage = info.url
+    
+    return html_template.format(**locals())
+
+html_template = """<html>
+<body><div align="center">
+<h1>{appname}</h1>
+<h3>{version}</h3>
+<p>{description}</p>
+<p>{copyright}</p>
+{translator}
+<p>{license}</p>
+<p><a href="{homepage}">{homepage}</a></p>
+</div></body>
+</html>
+"""
