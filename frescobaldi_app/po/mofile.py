f69aed8a08af2d04ddfe8aa683fea90fa865d121
Wilbert Berendsen <wbsoft@xs4all.nl>
2011-04-06 17:31:17 +0200

    much better own molint

--- frescobaldi_app/po/mofile.py
+++ frescobaldi_app/po/mofile.py
@@ -1,4 +1,5 @@
-# a loader for MO files
+# a loader for MO files, written by Wilbert Berendsen
+#
 # Heavily inspired by the Python gettext library, but allows for reading
 # all contents of a MO file, also the msgid2's, which are not needed
 # for translating but are useful when checking messages and translations
@@ -40,7 +41,9 @@ def parse_mo(buf):
             tmsg = buf[toff:tend]
         else:
             raise IOError(0, 'Corrupt MO data')
+        
         yield msg, tmsg
+        
         masteridx += 8
         transidx += 8
 
@@ -65,5 +68,37 @@ def parse_header(data):
             elif lastkey:
                 info[lastkey] += '\n' + line
     return info
+
+
+def parse_mo_split(buf):
+    """Parses the mo file and splits up all messages/translation pairs.
+    
+    Yields a three-tuple everytime: (contest, messages, translations)
+    
+    where context is None or bytes, and messages and translations both unencoded lists.
+    
+    """
+    for msg, tmsg in parse_mo(buf):
+        try:
+            context, msg = msg.split('\x04')
+        except ValueError:
+            context = None
+        yield context, msg.split('\x00'), tmsg.split('\x00')
+
+
+def parse_mo_decode(buf, default_charset="UTF-8"):
+    """Parses and splits, returns three-tuples like parse_mo_split but decoded to unicode."""
+    charset = default_charset
+    for context, msgs, tmsgs in parse_mo_split(buf):
+        if msgs[0] == '':
+            info = parse_header(tmsgs[0])
+            try:
+                dummy, charset = info.get('content-type', '').split('charset=')
+            except ValueError:
+                pass
+        yield (context.decode(charset) if context else None,
+               [msg.decode(charset) for msg in msgs],
+               [tmsg.decode(charset) for tmsg in tmsgs])
+
 
 
--- frescobaldi_app/po/molint.py
+++ frescobaldi_app/po/molint.py
@@ -14,7 +14,7 @@ import sys
 import mofile
 
 try:
-    mo = sys.argv[1]
+    filename = sys.argv[1]
 except IndexError:
     print("usage: python molint.py <mofile.mo>")
     sys.exit(1)
@@ -25,18 +25,45 @@ rx = re.compile(br"(?:^|[^{])\{([a-z]+)")
 def fields(text):
     return set(rx.findall(text))
 
-exitCode = 0
 
-for key, value in mofile.parse_mo(open(mo, 'rb').read()):
-    superfluous = fields(value) - fields(key)
-    if superfluous:
-        print('')
-        print("{0}: Translation contains fields that are not in message!".format(mofile))
-        print("  Message: {0}".format(key))
-        print("  Translation: {0}".format(value))
-        fieldlist = ["{{{0}}}".format(name) for name in superfluous]
-        print("  Field(s) not in message: {0}".format(", ".join(fieldlist)))
-        exitCode = 1
+def molint(filename):
+    """Checks filename for superfluous fields in the translated messages."""
+    exitCode = 0
+    for context, messages, translations in mofile.parse_mo_decode(open(filename, 'rb').read()):
+
+        # collect fields in messages
+        s = set()
+        for m in messages:
+            s |= fields(m)
+        
+        # collect superfluous fields in translations
+        errors = []
+        for t in translations:
+            superfluous = fields(t) - s
+            if superfluous:
+                errors.append((t, superfluous))
+        
+        # write out errors if any
+        if errors:
+            exitCode = 1
+            print('')
+            print("{0}: Translation contains fields that are not in message!".format(filename))
+            
+            print("  Message{0}:".format('' if len(messages) == 1 else "s"))
+            for m in messages:
+                print("    " + m)
+            
+            print("  Offending translation{0}:".format('' if len(errors) == 1 else "s"))
+            
+            totalsuperfluous = set()
+            for t, superfluous in errors:
+                print("    " + t)
+                totalsuperfluous |= superfluous
+            
+            fieldlist = ["{{{0}}}".format(name) for name in totalsuperfluous]
+            print("  Field(s) not in message: {0}".format(", ".join(fieldlist)))
+    
+    return exitCode
 
 
-sys.exit(exitCode)
+sys.exit(molint(filename))
