dbf9cfa1c7aa55afe6cb424fe73b6d7294e49ee1
Wilbert Berendsen <wbsoft@xs4all.nl>
2011-04-06 07:29:40 +0200

    molint.py checks for wrong format fields in translated messages

---
+++ frescobaldi_app/po/molint.py
@@ -0,0 +1,41 @@
+#! python
+
+"""
+This script reads a MO file and checks that translated messages do not use named
+string formatting fields that do not appear in the original messages.
+
+This would cause keyword errors when the format method is called on a translated
+string.
+"""
+
+import gettext
+import re
+import sys
+
+try:
+    mofile = sys.argv[1]
+except IndexError:
+    print("usage: python molint.py <mofile.mo>")
+    sys.exit(1)
+
+
+t = gettext.GNUTranslations(file(mofile))
+rx = re.compile(r"(?:^|[^{])\{([a-z]+)")
+
+def fields(text):
+    return set(rx.findall(text))
+
+# this trick works hopefully elsewhere as well...
+for key, value in t._catalog.items():
+    if isinstance(key, tuple):
+        key = key[0]
+    superfluous = fields(value) - fields(key)
+    if superfluous:
+        print('')
+        print("**** This translation contains fields that are not in the message! ****")
+        print("Message: {0}".format(key))
+        print("Translation: {0}".format(value))
+        print("Field(s) not in message: {0}".format(", ".join(superfluous)))
+
+
+
