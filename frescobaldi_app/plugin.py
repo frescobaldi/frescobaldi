9feeb02790ebb39d7a0960bd8e4d9ff7ff12122b
Wilbert Berendsen <wbsoft@xs4all.nl>
2011-04-09 08:40:21 +0200

    improve documentation and remove unused *args stuff

--- frescobaldi_app/plugin.py
+++ frescobaldi_app/plugin.py
@@ -19,6 +19,19 @@
 
 """
 Simple internal plugin api.
+
+A Plugin is like an extension of the object it is used for. You would
+normally subclass it to do useful things. It is created by calling the
+instance(obj) method and lives as long as the object lives. When calling
+instance(obj) again, the same Plugin instance is returned.
+
+The Plugin instance keeps a weak reference to its object in the _parent
+attribute. The object is unchanged, it knows nothing about the plugin.
+
+There are three often used Plugin classes defined here:
+MainWindowPlugin (for MainWindow instances), DocumentPlugin (for Document instances)
+and ViewSpacePlugin (for ViewSpace instances).
+
 """
 
 from __future__ import unicode_literals
@@ -34,20 +47,19 @@ class Plugin(object):
     A Plugin is coupled to another object and is automatically garbage collected
     as soon as the other object disappears.
     
-    Use the instance() class method to get/create one for an object.
+    Use the instance() class method to get/create the Plugin instance for an object.
     
     Implement the __init__() method if you want to do some setup.
-    This method then needs to accept the arguments you give to the instance()
-    class method.
     
     The instances() class method returns all living instances of this plugin type.
     
     """
-    def __init__(self, *args, **kwargs):
+    def __init__(self, obj):
+        """Implement this method to setup the plugin instance."""
         pass
     
     @classmethod
-    def instance(cls, obj, *args, **kwargs):
+    def instance(cls, obj):
         """Returns the instance of this plugin type for this object.
         
         The plugin instance is created if it did not exist.
@@ -57,9 +69,9 @@ class Plugin(object):
             return _instances[cls][obj]
         except KeyError:
             instances = _instances.setdefault(cls, weakref.WeakKeyDictionary())
-            result = instances[obj] = cls.__new__(cls, obj, *args, **kwargs)
+            result = instances[obj] = cls.__new__(cls, obj)
             result._parent = weakref.ref(obj)
-            result.__init__(obj, *args, **kwargs)
+            result.__init__(obj)
         return result
     
     @classmethod
@@ -74,18 +86,21 @@ class Plugin(object):
 class DocumentPlugin(Plugin):
     """Base class for plugins that live besides a Document."""
     def document(self):
+        """Returns the Document this plugin is used for."""
         return self._parent()
 
 
 class MainWindowPlugin(Plugin):
     """Base class for plugins that live besides a MainWindow."""
     def mainwindow(self):
+        """Returns the MainWindow this plugin is used for."""
         return self._parent()
 
 
 class ViewSpacePlugin(Plugin):
     """Base class for plugins that live besides a ViewSpace."""
     def viewSpace(self):
+        """Returns the ViewSpace this plugin is used for."""
         return self._parent()
 
 
