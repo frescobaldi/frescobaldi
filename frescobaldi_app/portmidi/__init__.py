e04fef8883239af861e0abb5455427689db4d607
Wilbert Berendsen <wbsoft@xs4all.nl>
2011-04-20 12:07:55 +0200

    start of embedding portmidi via ctypes

---
+++ frescobaldi_app/portmidi/__init__.py
@@ -0,0 +1,84 @@
+"""
+This packages accesses the low-level, cross-platform PortMidi library via ctypes.
+This file defines the (simple) API, loosely based on the PyGame.MIDI api.
+"""
+
+from .constants import *
+from .pm_ctypes import libpm, libpt, pmHostError, PmEvent, PortMidiStreamPtr
+
+
+def init():
+    """Initializes the PortMidi library.
+    
+    Must be called before anything else.
+    
+    """
+    libpm.Pm_Initialize()
+    # equiv to TIME_START: start timer w/ ms accuracy
+    libpt.Pt_Start(1, None, None)
+
+def quit():
+    """De-initializes the PortMidi library.
+
+    Call this to clean up Midi streams when done. If you do not call this on
+    Windows machines when you are done with MIDI, your system may crash.
+
+    """
+    libpm.Pm_Terminate()
+
+def count():
+    """Returns the number of available MIDI devices."""
+    return libpm.Pm_CountDevices()
+
+def device_info(device_id):
+    """Returns 5-item tuple parameters with information about the given device.
+
+    Returns a tuple with the following five elements:
+
+    - underlying MIDI API (str)
+    - device name (str)
+    - True if input is available (bool)
+    - True if output is available (bool)
+    - True if device stream is already open (bool)
+
+    """
+    info_ptr = libpm.Pm_GetDeviceInfo(device_id)
+    if info_ptr:
+        info = info_ptr.contents
+        return (info.interf, info.name, bool(info.input), bool(info.output),
+            bool(info.opened))
+
+def default_output_device():
+    """Return ID of the current default MID output device."""
+    return libpm.Pm_GetDefaultOutputDeviceID()
+
+def default_input_device():
+    """Return ID of the current default MIDI input device."""
+    return libpm.Pm_GetDefaultInputDeviceID()
+
+def time():
+    """Returns the current time of the PortMidi timer in milliseconds."""
+    return libpt.Pt_Time()
+
+def error_text(err_no):
+    """Return human-readable error message for the given error number."""
+    return libpm.Pm_GetErrorText(err_no)
+
+def channel(channel_no):
+    """Create a channel filter mask to use on MIDI input streams.
+
+    @param channel_no: MIDI channel number (1-16)
+    @type: int
+
+    Example: to receive input on channels 1 and 10 on a MIDI stream called
+    MidiIn::
+
+        MidiIn.set_channel_mask(pm.channel(1) | pm.channel(10))
+
+    .. note: The channel function has been altered from the original PortMidi C
+        function to correct for what seems to be a bug -- i.e. channels were
+        all numbered from 0 to 15 instead of 1 to 16.
+
+    """
+    return libpm.Pm_Channel(channel_no - 1)
+
---
+++ frescobaldi_app/portmidi/pm_ctypes.py
@@ -0,0 +1,238 @@
+# This script embeds the PortMidi Portable MIDI library via ctypes.
+# It is based on Grant Yoshida's 2007 version with some updates from Christopher Arndt from 2009
+# with some bugs fixed and restructured a bit.
+
+import os
+import sys
+
+from ctypes import (CDLL, CFUNCTYPE, POINTER, Structure, byref, c_char_p,
+    c_int, c_long, c_uint, c_void_p, cast, create_string_buffer)
+
+# the basename of the portmidi/porttime libraries on different platforms
+_PM_DLL = dict(
+    win32 = 'libportmidi-0',
+    linux2 = 'portmidi',
+    darwin = 'portmidi'
+)
+_PT_DLL = dict(
+    win32 = 'libporttime-0',
+    linux2 = 'porttime',
+    darwin = 'porttime'
+)
+
+if sys.platform.startswith('win'):
+    # ctypes.util.find_library() does not implement the full Windows DLL search
+    # order, so we have to provide it ourselves, so that the PortMidi DLL can
+    # be found. See the docstring of the find_library() function for more
+    # information.
+
+    from ctypes import windll, c_wchar_p, create_unicode_buffer
+
+    def get_system_directory():
+        """Return the path of the Windos system directory as a unicode string."""
+        try:
+            windll.kernel32.GetSystemDirectoryW.argtypes = [c_wchar_p, c_uint]
+            windll.kernel32.GetSystemDirectoryW.restype = c_uint
+        except AttributeError:
+            return None
+        else:
+            path = create_unicode_buffer(256)
+            plen = windll.kernel32.GetSystemDirectoryW(path, 256)
+            return plen and path.value or None
+
+    def get_windows_directory():
+        """Return the path of the Windos directory as a unicode string."""
+        try:
+            windll.kernel32.GetWindowsDirectoryW.argtypes = [c_wchar_p, c_uint]
+            windll.kernel32.GetWindowsDirectoryW.restype = c_uint
+        except AttributeError:
+            return None
+        else:
+            path = create_unicode_buffer(256)
+            plen = windll.kernel32.GetWindowsDirectoryW(path, 256)
+            return plen and path.value or None
+
+    def find_library(name, prepend_paths=None):
+        """Find and return the path of the given DLL using the DLL search order.
+
+        'name' should be the basename of the DLL with or without the '.dll'
+        extension. If 'prepend_paths' is specified, it should be a list of
+        directories to be searched before the default ones.
+
+        The default search order searches these directories:
+
+        - The directory from where the application (i.e. the main Python script)
+          is loaded
+        - The Windows system directory (e.g. C:\Windows\System32)
+        - The Windows 16-bit system directory (e.g. C:\Windows\System)
+        - The Windows directory (e.g. C:\Windows)
+        - The current directory
+        - Any directory named on the PATH environment variable
+
+        """
+        windir = get_windows_directory()
+        search_paths = (prepend_paths or []) + [
+            os.path.dirname(sys.argv[0]),
+            get_system_directory(),
+            os.path.join(windir, 'System'),
+            windir,
+            os.curdir
+        ] + [p for p in os.environ['PATH'].split(os.pathsep) if p]
+        for directory in search_paths:
+            fname = os.path.join(directory, name)
+            if os.path.exists(fname):
+                return fname
+            if fname.lower().endswith(".dll"):
+                continue
+            fname = fname + ".dll"
+            if os.path.exists(fname):
+                return fname
+        return None
+
+    dll_name = find_library(_PM_DLL['win32'], [os.path.dirname(__file__)])
+else:
+    from ctypes.util import find_library
+    dll_name = find_library(_PM_DLL.get(sys.platform, 'portmidi'))
+if dll_name is None:
+    raise ImportError("Couldn't find the PortMidi library.")
+
+libpm = CDLL(dll_name)
+# portmidi mightbe linked against porttime but not actually export its symbols
+# so we might need to load the porttime lib dynamically as well.
+try:
+    libpm.Pt_Time
+except AttributeError:
+    libpt = CDLL(find_library(_PT_DLL.get(sys.platform, 'porttime')))
+else:
+    libpt = libpm
+
+
+# portmidi.h
+
+PmError = c_int
+# PmError enum
+pmNoError = 0
+pmHostError = -10000
+pmInvalidDeviceId = -9999
+pmInsufficientMemory = -9998
+pmBufferTooSmall = -9997
+pmBufferOverflow = -9996
+pmBadPtr = -9995
+pmBadData = -9994
+pmInternalError = -9993
+pmBufferMaxSize = -9992
+
+libpm.Pm_Initialize.restype = PmError
+libpm.Pm_Terminate.restype = PmError
+
+PmDeviceID = c_int
+
+PortMidiStreamPtr = c_void_p
+PmStreamPtr = PortMidiStreamPtr
+PortMidiStreamPtrPtr = POINTER(PortMidiStreamPtr)
+
+libpm.Pm_HasHostError.restype = c_int
+libpm.Pm_HasHostError.argtypes = [PortMidiStreamPtr]
+
+libpm.Pm_GetErrorText.restype = c_char_p
+libpm.Pm_GetErrorText.argtypes = [PmError]
+
+libpm.Pm_GetHostErrorText.argtypes = [c_char_p, c_uint]
+
+pmNoDevice = -1
+
+class PmDeviceInfo(Structure):
+    _fields_ = [("structVersion", c_int),
+                ("interf", c_char_p),
+                ("name", c_char_p),
+                ("input", c_int),
+                ("output", c_int),
+                ("opened", c_int)]
+
+PmDeviceInfoPtr = POINTER(PmDeviceInfo)
+
+libpm.Pm_CountDevices.restype = c_int
+libpm.Pm_GetDefaultOutputDeviceID.restype = PmDeviceID
+libpm.Pm_GetDefaultInputDeviceID.restype = PmDeviceID
+
+PmTimestamp = c_long
+PmTimeProcPtr = CFUNCTYPE(PmTimestamp, c_void_p)
+NullTimeProcPtr = cast(None, PmTimeProcPtr)
+
+# PmBefore is not defined
+
+libpm.Pm_GetDeviceInfo.argtypes = [PmDeviceID]
+libpm.Pm_GetDeviceInfo.restype = PmDeviceInfoPtr
+
+libpm.Pm_OpenInput.restype = PmError
+libpm.Pm_OpenInput.argtypes = [PortMidiStreamPtrPtr,
+                             PmDeviceID,
+                             c_void_p,
+                             c_long,
+                             PmTimeProcPtr,
+                             c_void_p]
+
+libpm.Pm_OpenOutput.restype = PmError
+libpm.Pm_OpenOutput.argtypes = [PortMidiStreamPtrPtr,
+                             PmDeviceID,
+                             c_void_p,
+                             c_long,
+                             PmTimeProcPtr,
+                             c_void_p,
+                             c_long]
+
+libpm.Pm_SetFilter.restype = PmError
+libpm.Pm_SetFilter.argtypes = [PortMidiStreamPtr, c_long]
+
+libpm.Pm_SetChannelMask.restype = PmError
+libpm.Pm_SetChannelMask.argtypes = [PortMidiStreamPtr, c_int]
+
+libpm.Pm_Abort.restype = PmError
+libpm.Pm_Abort.argtypes = [PortMidiStreamPtr]
+
+libpm.Pm_Close.restype = PmError
+libpm.Pm_Close.argtypes = [PortMidiStreamPtr]
+
+PmMessage = c_long
+
+class PmEvent(Structure):
+    _fields_ = [("message", PmMessage),
+                ("timestamp", PmTimestamp)]
+
+PmEventPtr = POINTER(PmEvent)
+
+libpm.Pm_Read.restype = PmError
+libpm.Pm_Read.argtypes = [PortMidiStreamPtr, PmEventPtr, c_long]
+
+libpm.Pm_Poll.restype = PmError
+libpm.Pm_Poll.argtypes = [PortMidiStreamPtr]
+
+libpm.Pm_Write.restype = PmError
+libpm.Pm_Write.argtypes = [PortMidiStreamPtr, PmEventPtr, c_long]
+
+libpm.Pm_WriteShort.restype = PmError
+libpm.Pm_WriteShort.argtypes = [PortMidiStreamPtr, PmTimestamp, c_long]
+
+libpm.Pm_WriteSysEx.restype = PmError
+libpm.Pm_WriteSysEx.argtypes = [PortMidiStreamPtr, PmTimestamp, c_char_p]
+
+# porttime.h
+
+# PtError enum
+PtError = c_int
+ptNoError = 0
+ptHostError = -10000
+ptAlreadyStarted = -9999
+ptAlreadyStopped = -9998
+ptInsufficientMemory = -9997
+
+PtTimestamp = c_long
+PtCallback = CFUNCTYPE(PmTimestamp, c_void_p)
+
+libpt.Pt_Start.restype = PtError
+libpt.Pt_Start.argtypes = [c_int, PtCallback, c_void_p]
+
+libpt.Pt_Stop.restype = PtError
+libpt.Pt_Started.restype = c_int
+libpt.Pt_Time.restype = PtTimestamp
+
