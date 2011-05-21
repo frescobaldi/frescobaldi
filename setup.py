143bcb175355a5146ccb81d998dfe35b44cca044
wbsoft <wbsoft@xs4all.nl>
2011-05-21 08:14:44 +0200

    add package_dir, but package_data still seems to be ignored...

--- setup.py
+++ setup.py
@@ -9,6 +9,38 @@ def packagelist(directory):
         for root, dirs, files in os.walk(directory)
         if '__init__.py' in files))
 
+scripts = ['frescobaldi']
+if sys.platform.startswith('win'):
+	scripts.append('frescobaldi-postinstall.py')
+
+packages = packagelist('frescobaldi_app')
+package_dir = dict((p, p.replace('.', os.sep)) for p in packages)
+package_data = {
+	'frescobaldi_app.hyphdicts': ['*.dic'],
+	'frescobaldi_app.icons': [
+		'*.svg', '*x*/.png',
+		'tango/*.svg', 'tango/*x*/*.png',
+		],
+	'frescobaldi_app.po': ['*.mo'],
+	'frescobaldi_app.symbols': ['*.svg'],
+	'frescobaldi_app.postinstall': ['*.ico'],
+}
+data_files = [
+	('share/icons/hicolor/scalable/apps', ['frescobaldi_app/icons/frescobaldi.svg']),
+	('share/applications', ['frescobaldi.desktop']),
+]
+classifiers = [
+	'Development Status :: 3 - Alpha',
+	'Intended Audience :: End Users/Desktop',
+	'License :: OSI Approved :: GNU General Public License (GPL)',
+	'Operating System :: MacOS :: MacOS X',
+	'Operating System :: Microsoft :: Windows',
+	'Operating System :: POSIX',
+	'Programming Language :: Python',
+	'Topic :: Multimedia :: Sound/Audio',
+	'Topic :: Multimedia :: Graphics',
+]
+
 setup(
     name = info.name,
     version = info.version,
@@ -19,35 +51,11 @@ setup(
     url = info.url,
     license = info.license,
     
-    scripts = (
-        ['frescobaldi', 'frescobaldi-postinstall.py'] if sys.platform.startswith('win')
-        else ['frescobaldi']
-    ),
-    
-    packages = packagelist('frescobaldi_app'),
-    package_data = {
-        'frescobaldi_app.hyphdicts': ['*.dic'],
-        'frescobaldi_app.icons': [
-            '*.svg', '*x*/.png',
-            'tango/*.svg', 'tango/*x*/*.png',
-            ],
-        'frescobaldi_app.po': ['*.mo'],
-        'frescobaldi_app.symbols': ['*.svg'],
-		'frescobaldi_app.postinstall': ['*.ico'],
-    },
-    data_files = [
-        ('share/icons/hicolor/scalable/apps', ['frescobaldi_app/icons/frescobaldi.svg']),
-        ('share/applications', ['frescobaldi.desktop']),
-    ],
-    classifiers = [
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Topic :: Multimedia :: Sound/Audio',
-        'Topic :: Multimedia :: Graphics',
-    ],
+	scripts = scripts,
+	packages = packages,
+	package_dir = package_dir,
+	package_data = package_data,
+	data_files = data_files,
+	classifiers = classifiers,
 )
+
