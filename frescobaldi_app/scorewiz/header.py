3e6e056e9ea813daf4358a5e242d8a42b0a436f3
Wilbert Berendsen <wbsoft@xs4all.nl>
2011-07-06 12:39:25 +0200

    header dialog widget with score example png

---
+++ frescobaldi_app/scorewiz/header.py
@@ -0,0 +1,151 @@
+# This file is part of the Frescobaldi project, http://www.frescobaldi.org/
+#
+# Copyright (c) 2008 - 2011 by Wilbert Berendsen
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# See http://www.gnu.org/licenses/ for more information.
+
+"""
+The Header (titles) widget.
+"""
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+import app
+import textformats
+
+from . import __path__
+
+
+class HeaderWidget(QWidget):
+    def __init__(self, dialog):
+        super(HeaderWidget, self).__init__(dialog)
+        
+        layout = QHBoxLayout()
+        self.setLayout(layout)
+        
+        # The html view with the score layout example
+        t = self.htmlView = QTextBrowser()
+        t.setOpenLinks(False)
+        t.setOpenExternalLinks(False)
+        t.setSearchPaths(__path__)
+        t.setTextInteractionFlags(Qt.LinksAccessibleByMouse)
+
+        # ensure that the full HTML example page is displayed
+        t.setContentsMargins(2, 2, 2, 2)
+        t.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        t.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        t.setMinimumSize(QSize(350, 350))
+        layout.addWidget(t)
+        
+        t.anchorClicked.connect(self.slotAnchorClicked)
+        
+        grid = QGridLayout()
+        layout.addLayout(grid)
+        
+        grid.setVerticalSpacing(1)
+        grid.setColumnMinimumWidth(1, 200)
+        
+        self.labels = {}
+        self.edits = {}
+        for row, (name, desc) in enumerate(headers()):
+            l = QLabel()
+            e = QLineEdit()
+            l.setBuddy(e)
+            grid.addWidget(l, row, 0)
+            grid.addWidget(e, row, 1)
+            self.labels[name] = l
+            self.edits[name] = e
+            e.setObjectName(name)
+            
+        app.settingsChanged.connect(self.readSettings)
+        self.readSettings()
+        app.translateUI(self)
+        
+    def translateUI(self):
+        msg = _("Click to enter a value.")
+        self.htmlView.setHtml(titles_html.format(
+            copyrightmsg = _("bottom of first page"),
+            taglinemsg = _("bottom of last page"),
+            **dict((name, "<a title='{0}' href='{1}'>{2}</a>".format(msg, name, desc))
+                    for name, desc in headers())))
+        for name, desc in headers():
+            self.labels[name].setText(desc + ":")
+
+    def readSettings(self):
+        p = self.htmlView.palette()
+        p.setColor(QPalette.Base, textformats.formatData('editor').baseColors['paper'])
+        self.htmlView.setPalette(p)
+    
+    def slotAnchorClicked(self, url):
+        try:
+            e = self.edits[url.toString()]
+        except KeyError:
+            return
+        e.setFocus()
+
+
+
+def headers():
+    yield 'dedication',  _("Dedication")
+    yield 'title',       _("Title")
+    yield 'subtitle',    _("Subtitle")
+    yield 'subsubtitle', _("Subsubtitle")
+    yield 'instrument',  _("Instrument")
+    yield 'composer',    _("Composer")
+    yield 'arranger',    _("Arranger")
+    yield 'poet',        _("Poet")
+    yield 'meter',       _("Meter")
+    yield 'piece',       _("Piece")
+    yield 'opus',        _("Opus")
+    yield 'copyright',   _("Copyright")
+    yield 'tagline',     _("Tagline")
+
+
+titles_html = r"""<html><head><style type='text/css'>
+body {{
+  color: black;
+}}
+a {{
+  text-decoration: none;
+  color: black;
+}}
+</style></head>
+<body><table width='100%' style='font-family:serif;'>
+<tr><td colspan=3 align=center>{dedication}</td></tr>
+<tr><td colspan=3 align=center style='font-size:20pt;'><b>{title}</b></td></tr>
+<tr><td colspan=3 align=center style='font-size:12pt;'><b>{subtitle}</b></td></tr>
+<tr><td colspan=3 align=center><b>{subsubtitle}</b></td></tr>
+<tr>
+    <td align=left width='25%'>{poet}</td>
+    <td align=center><b>{instrument}</b></td>
+    <td align=right width='25%'>{composer}</td>
+</tr>
+<tr>
+    <td align=left>{meter}</td>
+    <td> </td>
+    <td align=right>{arranger}</td>
+</tr>
+<tr>
+    <td align=left>{piece}</td>
+    <td> </td>
+    <td align=right>{opus}</td>
+</tr>
+<tr><td colspan=3 align=center><img src='scorewiz.png'></td></tr>
+<tr><td colspan=3 align=center>{copyright} <i>({copyrightmsg})</i></td></tr>
+<tr><td colspan=3 align=center>{tagline} <i>({taglinemsg})</i></td></tr>
+</table></body></html>
+"""
---
+++ frescobaldi_app/scorewiz/scorewiz.ly
@@ -0,0 +1,22 @@
+\version "2.14"
+
+\paper {
+  paper-height = 1\in
+  paper-width = 3\in
+  evenHeaderMarkup = ##f
+  oddHeaderMarkup = ##f
+  evenFooterMarkup = ##f
+  oddFooterMarkup = ##f
+  
+  ragged-right = ##f
+  indent = #0
+  
+  left-margin = #0
+  top-margin = #1
+  right-margin = #0
+  bottom-margin = #0
+}
+
+\relative c' {
+  c d e f g a b c
+}
