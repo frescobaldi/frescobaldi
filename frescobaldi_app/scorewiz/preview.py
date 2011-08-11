c34c7689a6880dedfb8baeb1ab12155ca3cd67c4
Wilbert Berendsen <wbsoft@xs4all.nl>
2011-08-11 22:51:47 +0200

    example music works

--- frescobaldi_app/scorewiz/preview.py
+++ frescobaldi_app/scorewiz/preview.py
@@ -21,8 +21,13 @@
 The preview widget.
 """
 
+import itertools
+import math
+
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+
+import ly.dom
 
 from . import build
 
@@ -30,5 +35,103 @@ from . import build
 def preview(scorewiz):
     builder = build.Builder(scorewiz)
     
-    # fill the document with example notes
+    # get the document's assignments
+    doc = builder.document()
+    examplify(doc)
     
+    # TEMP!!
+    print builder.text(doc)
+
+
+
+def examplify(doc):
+    """Fills in some example music in the given ly.dom.Document."""
+    stubs = []
+    globals = {}
+    for a in doc.findChildren(ly.dom.Assignment, 2):
+        if isinstance(a.name, ly.dom.Reference):
+            g = None
+            for i in a.findChildren(ly.dom.Identifier):
+                if not isinstance(i.name, ly.dom.Reference):
+                    g = i.name
+                    break
+            stubs.append((g, a[-1]))
+        else:
+            keysig = a.findChild(ly.dom.KeySignature) 
+            timesig = a.findChild(ly.dom.TimeSignature)
+            partial = a.findChild(ly.dom.Partial)
+            
+            # create a list of durations for the example notes.
+            durations = []
+            if partial:
+                durations.append((partial.dur, partial.dots))
+            if timesig:
+                dur = int(math.log(int(timesig.beat), 2))
+                num = min(int(timesig.num)*2, 10)
+            else:
+                dur, num = 2, 4
+            durations += [(dur, 0)] * num
+
+            globals[a.name] = (keysig, durations)
+    
+    # other lyrics on each request
+    lyrics = itertools.cycle('ha hi he ho hu'.split())
+    
+    # fill assignment stubs with suitable example music input
+    num = 10
+    for g, stub in stubs:
+        try:
+            keysig, durations = globals[g]
+            num = len(durations)
+        except KeyError:
+            pass
+        
+        def addItems(stub, generator):
+            for dur, dots in durations:
+                node = next(generator)
+                node.append(ly.dom.Duration(dur, dots))
+                stub.append(node)
+        
+        if isinstance(stub, ly.dom.LyricMode):
+            stub.append(ly.dom.Text(' '.join(itertools.repeat(next(lyrics), num))))
+        elif isinstance(stub, ly.dom.Relative):
+            addItems(stub[-1], pitchGen(keysig))
+        elif isinstance(stub, ly.dom.ChordMode):
+            addItems(stub, chordGen(keysig))
+        elif isinstance(stub, ly.dom.FigureMode):
+            addItems(stub, figureGen())
+        elif isinstance(stub, ly.dom.DrumMode):
+            addItems(stub, drumGen())
+
+
+# Generators for different kinds of example input
+def pitchGen(startPitch):
+    note = startPitch.note
+    while True:
+        for n in (note, note, (note + 9 ) % 7, (note + 8) % 7,
+                  note, (note + 11) % 7, note):
+            chord = ly.dom.Chord()
+            ly.dom.Pitch(-1, n, startPitch.alter, parent=chord)
+            yield chord
+
+
+def chordGen(startPitch):
+    for n in pitchGen(startPitch):
+        yield n
+        for i in 1, 2, 3:
+            yield ly.dom.TextDur("\\skip")
+        
+
+def figureGen():
+    while True:
+        for i in 5, 6, 3, 8, 7:
+            for s in "<{0}>".format(i), "\\skip", "\\skip":
+                yield ly.dom.TextDur(s)
+            
+
+def drumGen():
+    while True:
+        for s in "bd", "hh", "sn", "hh":
+            yield ly.dom.TextDur(s)
+
+
