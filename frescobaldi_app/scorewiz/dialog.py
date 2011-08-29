0ef9c96016bf582f36fb90c377c14e229d283510
Wilbert Berendsen <wbsoft@xs4all.nl>
2011-08-29 19:20:17 +0200

    document scorewiz better

--- frescobaldi_app/scorewiz/dialog.py
+++ frescobaldi_app/scorewiz/dialog.py
@@ -195,32 +195,57 @@ class scorewiz_help(help.page):
         d['key'] = help.shortcut(action)
         d['menu'] = help.menu(_("menu title", "Tools"), _("Setup New Score..."))
         return _("""\
-<p>The Score Setup Wizard ({key}) in {menu} is designed
-to quickly setup a LilyPond music score.</p>
+<p>
+The Score Setup Wizard ({key}) in {menu} is designed
+to quickly setup a LilyPond music score.
+</p>
 
-<p>In the first tab, <em>Titles and Headers</em>, you can enter titling
-information.</p>
+<p>
+In the first tab, <em>Titles and Headers</em>, you can enter titling
+information.
+</p>
 
-<p>In the second tab, <em>Parts</em>, you can compose your score out of many
+<p>
+In the second tab, <em>Parts</em>, you can compose your score out of many
 available part types.
 Doubleclick a part type to add it to your score (or click Add).
 Select the part in the score list to change some settings for the selected part,
 if desired.
 Many parts, especially Choir, have powerful options to set up the score the way
-you want it.</p>
+you want it.
+</p>
 
-<p>The "Containers" category in the part types list contains the Score, Book and
-Bookpart types, with which you can setup a LilyPond document containing multiple
-scores or even books.</p>
+<p>
+In the third tab, <em>Score settings</em>, global score properties and
+preferences can be set.
+</p>
 
-<p>In the score view, you can add musical parts to a Score object.
+<p>
+Click the Preview button to get a preview with some example music filled in.
+Click OK to copy the generated LilyPond source text to the editor.
+</p>
+
+<h3>Multiple pieces or mouvements</h3>
+
+<p>
+A special and powerful feature of the <em>Parts</em> tab is hidden in the 
+"Containers" category in the part types list.
+</p>
+
+<p>
+This category contains the Score, Book and Bookpart types, with which you
+can setup a LilyPond document containing multiple scores or even books.
+You may add Score, Bookpart or Book entries to the score view.
+They can be nested: a Score can be added to a Bookpart or Book but you can't
+add a Book to a Bookpart or a Score.
+</p>
+
+<p>
+Then you can add musical parts.
 If you want to create multiple scores with exact the same parts, you can just
 add the parts to the top level of the score view, and then the scores, without
 adding musical parts to the scores.
-The scores will then use the parts in the top level of the score.</p>
-
-<p>In the third tab, <em>Score settings</em>, global score properties and
-preferences can be set.
-Click the Preview button to get a preview with some example music filled in.</p>
+The scores will then use the parts in the top level of the score.
+</p>
 """).format(**d)
 
