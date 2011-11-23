c517e1b30c8c62be535e9d3b394af8159d2ad1df
Wilbert Berendsen <wbsoft@xs4all.nl>
2011-11-23 16:13:57 +0100

    Documentation class representing one instance of Lily documentation

--- frescobaldi_app/helpbrowser/lilydoc.py
+++ frescobaldi_app/helpbrowser/lilydoc.py
@@ -24,14 +24,29 @@ Manage access to LilyPond documentation.
 from __future__ import unicode_literals
 
 import os
+import re
 
-from PyQt4.QtCore import QSettings, QUrl
+from PyQt4.QtCore import pyqtSignal, QObject, QSettings, QUrl
 
+import network
 import util
 
 
 def urls():
-    """Returns a list of QUrls where documentation can be found."""
+    """Returns a list of QUrls where documentation can be found.
+    
+    Remote urls (from the users settings) are not checked but simply returned.
+    For user-set local directories, if the directory itself does not contain
+    LilyPond documentation, all directories one level deep are searched.
+    
+    This makes it possible to set one directory for local documentation and
+    put there multiple sets of documentation in subdirectories (e.g. with the
+    version number in the path name).
+    
+    The paths in the settings are read, and also the usual system directories
+    are scanned.
+    
+    """
     user_paths = QSettings().value("documentation/paths", []) or []
     system_prefixes = [p for p in (
         '/usr',
@@ -60,6 +75,7 @@ def urls():
             if os.path.isdir(os.path.join(p, 'Documentation')):
                 return p
     
+    # search in the user-set directories, if no docs, scan one level deeper
     for p in user_prefixes:
         n = find(p)
         if n:
@@ -71,6 +87,7 @@ def urls():
                     if n:
                         local.append(n)
                 break
+    # now add the system directories if documentation is found there
     for p in system_prefixes:
         if p not in user_prefixes:
             n = find(p)
@@ -80,5 +97,68 @@ def urls():
     urls.extend(map(QUrl.fromLocalFile, local))
     urls.extend(map(QUrl, remote))
     return urls
+
+
+
+class Documentation(QObject):
+    """An instance of LilyPond documentation."""
+    versionLoaded = pyqtSignal(bool)
+    
+    def __init__(self, url):
+        QObject.__init__(self)
+        self._url = url
+        self._localFile = url.toLocalFile()
+        self._versionString = None
+        
+        # determine version
+        url = self.url()
+        sep = '/' if not url.path().endswith('/') else ''
+        url.setPath(url.path() + sep + 'VERSION')
+        reply = self._versionReply = network.get(url)
+        if reply.isFinished():
+            self._versionReplyFinished()
+        else:
+            reply.finished.connect(self._versionReplyFinished)
+    
+    def url(self):
+        return QUrl(self._url)
+    
+    def home(self):
+        """Returns the url with 'Documentation' appended."""
+        url = self.url()
+        sep = '/' if not url.path().endswith('/') else ''
+        url.setPath(url.path() + sep + 'Documentation')
+        return url
+    
+    def _versionReplyFinished(self):
+        if self._versionReply.error():
+            self._versionString = ''
+        else:
+            self._versionString = bytes(self._versionReply.readAll()).strip()
+        self.versionLoaded.emit(bool(self._versionString))
+        self._versionReply.deleteLater()
+    
+    def versionString(self):
+        """Returns the version as a string.
+        
+        If the version is not yet determined, returns None.
+        If the version could not be determined, returns the empty string.
+        
+        """
+        return self._versionString
+    
+    def version(self):
+        """Returns the version as a tuple of ints.
+        
+        If the version is not yet determined, returns None.
+        If the version could not be determined, returns the empty tuple.
+        
+        """
+        if self._versionString is not None:
+            return tuple(map(int, re.findall(br"\d+", self._versionString)))
+    
+    def isLocal(self):
+        """Returns True if the documentation is on the local system."""
+        return bool(self._localFile)
 
 
