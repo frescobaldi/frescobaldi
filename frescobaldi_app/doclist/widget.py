d8c333969f1a58a50b9f6c520973d146ce36a9ff
Wilbert Berendsen <wbsoft@xs4all.nl>
2011-12-16 20:11:18 +0100

    setting for document grouping now works correctly

--- frescobaldi_app/doclist/widget.py
+++ frescobaldi_app/doclist/widget.py
@@ -51,11 +51,7 @@ def path(url):
 class Widget(QTreeWidget):
     def __init__(self, tool):
         super(Widget, self).__init__(tool, headerHidden=True)
-        self._paths = {}
-        self._items = {}
-        
-        self.readSettings()
-        app.settingsChanged.connect(self.readSettingsAgain)
+        self.setAlternatingRowColors(True)
         app.documentCreated.connect(self.addDocument)
         app.documentClosed.connect(self.removeDocument)
         app.documentLoaded.connect(self.setDocumentStatus)
@@ -63,23 +59,25 @@ class Widget(QTreeWidget):
         app.documentUrlChanged.connect(self.setDocumentStatus)
         app.jobStarted.connect(self.setDocumentStatus)
         app.jobFinished.connect(self.setDocumentStatus)
-       
         tool.mainwindow().currentDocumentChanged.connect(self.selectDocument)
-        
-        # add all existing docs to the list
-        for d in app.documents:
-            self.addDocument(d)
-        doc = tool.mainwindow().currentDocument()
-        if doc:
-            self.selectDocument(doc)
         self.currentItemChanged.connect(self.slotItemActivated)
-	
-    def readSettings(self):
+        app.settingsChanged.connect(self.populate)
+        self.populate()
+    
+    def populate(self):
         self._group = QSettings().value(
             "document_list/group_by_folder", False) in (True, "true")
-    
-    def readSettingsAgain(self):
-        self.readSettings()
+        self.setRootIsDecorated(not self._group)
+        self.clear()
+        self._paths = {}
+        self._items = {}
+        with util.signalsBlocked(self):
+            # add all existing docs to the list
+            for d in app.documents:
+                self.addDocument(d)
+            doc = self.parentWidget().mainwindow().currentDocument()
+            if doc:
+                self.selectDocument(doc)
         
     def addDocument(self, doc):
         self._items[doc] = QTreeWidgetItem(self)
@@ -87,16 +85,22 @@ class Widget(QTreeWidget):
     
     def removeDocument(self, doc):
         i = self._items.pop(doc)
-        self.takeTopLevelItem(self.indexOfTopLevelItem(i))
-        self.sortItems(0, Qt.AscendingOrder)
-
+        if not self._group:
+            self.takeTopLevelItem(self.indexOfTopLevelItem(i))
+            return
+        parent = i.parent()
+        parent.takeChild(parent.indexOfChild(i))
+        if parent.childCount() == 0:
+            self.takeTopLevelItem(self.indexOfTopLevelItem(parent))
+            del self._paths[parent._path]
+    
     def selectDocument(self, doc):
         self.setCurrentItem(self._items[doc])
 
     def setDocumentStatus(self, doc):
         i = self._items[doc]
+        # set properties according to document
         i.setText(0, doc.documentName())
-        # icon
         if jobmanager.isRunning(doc):
             icon = 'lilypond-run'
         elif doc.isModified():
@@ -105,8 +109,38 @@ class Widget(QTreeWidget):
             icon = 'text-plain'
         i.setIcon(0, icons.get(icon))
         i.setToolTip(0, path(doc.url()))
-        self.sortItems(0, Qt.AscendingOrder)
+        # handle ordering in groups if desired
+        if self._group:
+            self.groupDocument(doc)
+        else:
+            self.sortItems(0, Qt.AscendingOrder)
     
+    def groupDocument(self, doc):
+        """Called, if grouping is enabled, to group the document."""
+        i = self._items[doc]
+        p = path(doc.url())
+        new_parent = self._paths.get(p)
+        if new_parent is None:
+            new_parent = self._paths[p] = QTreeWidgetItem(self)
+            new_parent._path = p
+            new_parent.setText(0, p or _("Untitled"))
+            new_parent.setIcon(0, icons.get("folder-open"))
+            new_parent.setFlags(Qt.ItemIsEnabled)
+            new_parent.setExpanded(True)
+            self.sortItems(0, Qt.AscendingOrder)
+        old_parent = i.parent()
+        if old_parent == new_parent:
+            return
+        if old_parent:
+            old_parent.takeChild(old_parent.indexOfChild(i))
+            if old_parent.childCount() == 0:
+                self.takeTopLevelItem(self.indexOfTopLevelItem(old_parent))
+                del self._paths[old_parent._path]
+        else:
+            self.takeTopLevelItem(self.indexOfTopLevelItem(i))
+        new_parent.addChild(i)
+        new_parent.sortChildren(0, Qt.AscendingOrder)
+        
     def slotItemActivated(self, item):
         for d, i in self._items.items():
             if i == item:
