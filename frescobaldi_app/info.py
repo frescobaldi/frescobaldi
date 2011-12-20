ac8872eafef46848419835834f5587d81a67adb0
Wilbert Berendsen <wbsoft@xs4all.nl>
2011-12-20 14:28:22 +0100

    bump version to 2.0; make stable and add some classifiers to setup, rest is po noise

--- frescobaldi_app/info.py
+++ frescobaldi_app/info.py
@@ -26,7 +26,7 @@ from __future__ import unicode_literals
 
 # these variables are also used by the distutils setup
 name = "frescobaldi"
-version = "1.9.5"
+version = "2.0.0"
 description = "LilyPond Music Editor"
 long_description = \
     "Frescobaldi is an advanced text editor to edit LilyPond sheet music files. " \
