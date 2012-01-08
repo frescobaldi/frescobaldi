699fbec057d5cead3e0f3d203c1a0f694d1ec826
wbsoft <wbsoft@xs4all.nl>
2012-01-08 12:23:37 +0100

    frozen frescobaldi now works!

--- freeze.py
+++ freeze.py
@@ -24,6 +24,7 @@ includes = [
     'PyQt4.QtWebKit',
     'PyQt4.QtNetwork',
     'PyQt4.QtSvg',
+    'PyQt4.QtXml',
     'popplerqt4',
     'pypm',
     
@@ -72,15 +73,27 @@ f = Freezer(
 
 f.Freeze()
 
-# copy over pyqt4 imageformat plugins
+# copy PyQt4 imageformat plugins
 file_handle, path, desc = imp.find_module('PyQt4')
 img_formats = os.path.join(path, 'plugins', 'imageformats')
 img_formats_target = os.path.join(target_dir, 'imageformats')
 shutil.rmtree(img_formats_target, ignore_errors = True)
 shutil.copytree(img_formats, img_formats_target)
 
-# copy the frescobaldi_app directory, TODO: bytecompile?
+# copy the frescobaldi_app directory
 f_app = os.path.join(target_dir, 'frescobaldi_app')
 shutil.rmtree(f_app, ignore_errors=True)
 shutil.copytree('frescobaldi_app', f_app, ignore=shutil.ignore_patterns('*~'))
+
+# bytecompile frescobaldi_app
+import py_compile
+current_dir = os.getcwd()
+os.chdir(target_dir)
+for root, dirs, files in os.walk('frescobaldi_app'):
+    for f in files:
+        if f.endswith('.py'):
+            f = os.path.join(root, f)
+            sys.stdout.write('Byte-compiling %s\n' % f)
+            py_compile.compile(f)
+os.chdir(current_dir)
 
