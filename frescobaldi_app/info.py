caaa31f5feca311490df07195b313d1cac0f8c9c
Wilbert Berendsen <wbsoft@xs4all.nl>
2012-02-17 22:00:41 +0100

    ver bump to 2.0.4 and pot update

--- frescobaldi_app/info.py
+++ frescobaldi_app/info.py
@@ -26,7 +26,7 @@ from __future__ import unicode_literals
 
 # these variables are also used by the distutils setup
 name = "frescobaldi"
-version = "2.0.3"
+version = "2.0.4"
 description = "LilyPond Music Editor"
 long_description = \
     "Frescobaldi is an advanced text editor to edit LilyPond sheet music files. " \
