524a567dbf9e10d0143ebf12dba327619635c5af
Wilbert Berendsen <wbsoft@xs4all.nl>
2013-02-25 13:03:57 +0100

    let colorize use the new highlight2html module

--- frescobaldi_app/colorize.py
+++ frescobaldi_app/colorize.py
@@ -26,7 +26,7 @@ from __future__ import unicode_literals
 from PyQt4.QtGui import QTextFormat
 
 import ly.lex
-import highlighter
+import highlight2html
 import textformats
 
 
@@ -37,39 +37,18 @@ def colorize(text, state=None):
     """Converts the text to HTML using the specified or guessed state."""
     if state is None:
         state = ly.lex.guessState(text)
-    result = []
-    h = highlighter.highlightFormats()
-    d = textformats.formatData('editor')
-    result.append('<pre style="color: {0}; background: {1}; '
-        'font-family: &quot;{2}&quot;; font-size: {3}pt;">'.format(
-        d.baseColors['text'].name(), d.baseColors['background'].name(),
-        d.font.family(), d.font.pointSizeF()))
-    for t in state.tokens(text):
-        f = h.format(t)
-        if f:
-            s = style(f)
-            if s:
-                result.append('<span style="{0}">{1}</span>'.format(s, escape(t)))
-                continue
-        result.append(escape(t))
+    data = textformats.formatData('editor')
+    h = highlight2html.HtmlHighlighter(data)
+    h.inline_style = True
+    
+    result = [
+        '<pre style="color: {0}; background: {1}; font-family: {2}">'.format(
+        data.baseColors['text'].name(),
+        data.baseColors['background'].name(),
+        data.font.family())]
+    
+    result.extend(map(h.html_for_token, state.tokens(text)))
     result.append('</pre>')
     return ''.join(result)
-
-    
-def escape(text):
-    return text.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
-
-
-def style(f):
-    s = []
-    if f.hasProperty(QTextFormat.ForegroundBrush):
-        s.append('color: {0};'.format(f.foreground().color().name()))
-    if f.hasProperty(QTextFormat.BackgroundBrush):
-        s.append('color: {0};'.format(f.background().color().name()))
-    if f.hasProperty(QTextFormat.FontWeight):
-        s.append('font-weight: {0};'.format(f.fontWeight() * 8))
-    if f.fontItalic():
-        s.append('font-style: italic;')
-    return ' '.join(s)
 
 
