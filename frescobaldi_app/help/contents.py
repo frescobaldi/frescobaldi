beadef118d234fdda2036450eec820bacecd8926
Wilbert Berendsen <wbsoft@xs4all.nl>
2013-10-02 20:54:00 +0200

    typos

--- frescobaldi_app/help/contents.py
+++ frescobaldi_app/help/contents.py
@@ -661,7 +661,7 @@ class eng_preview(page):
               _("This mode colors items whose directions have been explicitly set "
                 "with either the predefined commands <code>\\xxxUp</code> etc. "
                 "or the directional operators <code>^</code> and <code>_</code>.<br />"
-                "Please note how this mode and the previous are related:</br>"
+                "Please note how this mode and the previous are related:<br />"
                 "When the condition for one of the the modes is reverted using "
                 "commands like <code>\\oneVoice</code> or <code>\\xxxNeutral</code> "
                 "colors are reverted to black and may also revert the highlighting "
@@ -719,7 +719,7 @@ class eng_publication(page):
         return p(
           _("When engraving a score in Publication mode the point-and-click "
             "information isn't generated. This will prevent a reader of the "
-            "resulting PDF document from navigating to the corresponging "
+            "resulting PDF document from navigating to the corresponding "
             "line in the input file, but it will also significantly decrease "
             "the file size."),
           _("You should also note that the point-and-click links contain "
