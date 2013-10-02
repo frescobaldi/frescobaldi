1751e7c613701c6fb72dca15859c08b50dd3eb8e
Wilbert Berendsen <wbsoft@xs4all.nl>
2013-10-02 14:44:40 +0200

    avoid structural markup in new translatable strings

--- frescobaldi_app/help/contents.py
+++ frescobaldi_app/help/contents.py
@@ -640,73 +640,75 @@ class eng_preview(page):
             "to have the panel constantly open because you will probably "
             "switch Debug Modes quite often."),
           _("The following Debug Modes are currently implemented:"),
-        ) + ul(li(
-          _("<code>Display Control Points</code><br />"
-            "Slurs, Ties and other similar objects are drawn in LilyPond "
-            "as third-order Bezier curves, which means that their shape "
-            "is controlled by four \"control-points\" (first and last ones "
-            "tell where the curve ends are placed, and the middle ones affect "
-            "the curvature). Changing the shape of these objects involves "
-            "moving these control-points around, and it's helpful to see "
-            "where they actually are.<br />"
-            "This Debug Mode will display the inner control-points "
-            "as red crosses and connects them to the outer (starting) "
-            "points with thin lines."),
-          _("<code>Color \\voiceXXX</code><br />"
-            "This mode highlights voices that have been explicitly set with "
-            "one of the <code>\\voiceXXX</code> commands. This is useful "
-            "when dealing with polyphony issues."),
-          _("<code>Color explicit directions</code><br />"
-            "This mode colors items whose directions have been explicitly set "
-            "with either the predefined commands <code>\\xxxUp</code> etc. "
-            "or the directional operators <code>^</code> and <code>_</code>.<br />"
-            "Please note how this mode and the previous are related:</br>"
-            "When the condition for one of the the modes is reverted using "
-            "commands like <code>\\oneVoice</code> or <code>\\xxxNeutral</code> "
-            "colors are reverted to black and may also revert the highlighting "
-            "of the other Debug Mode.<br />"
-            "In <code>c \\voiceTwo d \\stemUp e \\stemNeutral d \\oneVoice</code> "
-            "the \'d\' may already be reverted.<br />"
-            "If the score is engraved with LilyPond version 2.17.6 or later "
-            "the situation is somewhat improved through the use of the "
-            "<code>\\temporary</code> directive.<br />"
-            "In short: If you use both Modes at the same time please expect inconsistencies."),
-          _("<code>Display Grob Anchors</code><br />"
-            "In LilyPond, all graphical objects have an anchor (a reference point). "
-            "What is a reference point?  It's a special point that defines the "
-            "object's position.  Think about geometry: if you have to define where a "
-            "figure is placed on a plane, you'll usually say something like "
-            "\"the lower left corner of this square has coordinates (0, 2)\" or \"the "
-            "center of this circle is at (-1, 3)\". \"Lower left corner\" and \"center\" "
-            "would be the reference points for square and circle.<br />"
-            "This Mode displays a red dot for each grob's anchor point."),
-          _("<code>Display Grob Names</code><br />"
-            "This mode prints a grob's name next to it.<br />"
-            "The main purpose of this Debug Mode is to retrieve information about "
-            "Grob names, which may come in handy if you don\'t know where to "
-            "look up available properties.<br />"
-            "Please note that this mode is quite intrusive and will especially "
-            "become useful once it can be activated for single grobs."),
-          _("<code>Display Skylines</code><br />"
-            "LilyPond uses \"Skylines\" to calculate the vertical dimensions "
-            "of its graphical objects in order to prevent collisions. "
-            "This mode draws lines representing the skylines and is useful when "
-            "dealing with issues of vertical spacing."),
-          _("<code>Debug Paper Columns</code><br />"
-            "LilyPond organises the horizontal spacing in \"paper columns\". "
-            "This mode prints a lot of spacing information about these entities."),
-          _("<code>Annotate Spacing</code><br />"
-            "LilyPond has a built-in function that prints lots of information "
-            "about distances on the paper, which is very useful when debugging "
-            "the page layout."),
-          _("<code>Include Custom File</code><br />"
-            "This mode offers the opportunity to add one\'s own Debug Modes "
-            "by including a custom file. This file will be included at program "
-            "startup and may contain any LilyPond code you would like to have "
-            "executed whenever you are engraving in Preview Mode.</br>"
-            "The given string will be literally included in an <code>\\include</code> "
-            "directive, so you are responsible yourself that LilyPond can find it."),
-        ))
+        ) + ul(''.join("<li><b>{0}</b><br />{1}</li>\n".format(name, text)
+                for name, text in (
+             (_("Display Control Points"),
+              _("Slurs, Ties and other similar objects are drawn in LilyPond "
+                "as third-order Bezier curves, which means that their shape "
+                "is controlled by four \"control-points\" (first and last ones "
+                "tell where the curve ends are placed, and the middle ones affect "
+                "the curvature). Changing the shape of these objects involves "
+                "moving these control-points around, and it's helpful to see "
+                "where they actually are.<br />"
+                "This Debug Mode will display the inner control-points "
+                "as red crosses and connects them to the outer (starting) "
+                "points with thin lines.")),
+             (_("Color \\voiceXXX"),
+              _("This mode highlights voices that have been explicitly set with "
+                "one of the <code>\\voiceXXX</code> commands. This is useful "
+                "when dealing with polyphony issues.")),
+             (_("Color explicit directions"),
+              _("This mode colors items whose directions have been explicitly set "
+                "with either the predefined commands <code>\\xxxUp</code> etc. "
+                "or the directional operators <code>^</code> and <code>_</code>.<br />"
+                "Please note how this mode and the previous are related:</br>"
+                "When the condition for one of the the modes is reverted using "
+                "commands like <code>\\oneVoice</code> or <code>\\xxxNeutral</code> "
+                "colors are reverted to black and may also revert the highlighting "
+                "of the other Debug Mode.<br />"
+                "In <code>c \\voiceTwo d \\stemUp e \\stemNeutral d \\oneVoice</code> "
+                "the \'d\' may already be reverted.<br />"
+                "If the score is engraved with LilyPond version 2.17.6 or later "
+                "the situation is somewhat improved through the use of the "
+                "<code>\\temporary</code> directive.<br />"
+                "In short: If you use both Modes at the same time please expect "
+                "inconsistencies.")),
+             (_("Display Grob Anchors"),
+              _("In LilyPond, all graphical objects have an anchor (a reference point). "
+                "What is a reference point?  It's a special point that defines the "
+                "object's position.  Think about geometry: if you have to define where a "
+                "figure is placed on a plane, you'll usually say something like "
+                "\"the lower left corner of this square has coordinates (0, 2)\" or \"the "
+                "center of this circle is at (-1, 3)\". \"Lower left corner\" and \"center\" "
+                "would be the reference points for square and circle.<br />"
+                "This Mode displays a red dot for each grob's anchor point.")),
+             (_("Display Grob Names"),
+              _("This mode prints a grob's name next to it.<br />"
+                "The main purpose of this Debug Mode is to retrieve information about "
+                "Grob names, which may come in handy if you don\'t know where to "
+                "look up available properties.<br />"
+                "Please note that this mode is quite intrusive and will especially "
+                "become useful once it can be activated for single grobs.")),
+             (_("Display Skylines"),
+              _("LilyPond uses \"Skylines\" to calculate the vertical dimensions "
+                "of its graphical objects in order to prevent collisions. "
+                "This mode draws lines representing the skylines and is useful when "
+                "dealing with issues of vertical spacing.")),
+             (_("Debug Paper Columns"),
+              _("LilyPond organises the horizontal spacing in \"paper columns\". "
+                "This mode prints a lot of spacing information about these entities.")),
+             (_("Annotate Spacing"),
+              _("LilyPond has a built-in function that prints lots of information "
+                "about distances on the paper, which is very useful when debugging "
+                "the page layout.")),
+             (_("Include Custom File"),
+              _("This mode offers the opportunity to add one\'s own Debug Modes "
+                "by including a custom file. This file will be included at program "
+                "startup and may contain any LilyPond code you would like to have "
+                "executed whenever you are engraving in Preview Mode.</br>"
+                "The given string will be literally included in an <code>\\include</code> "
+                "directive, so you are responsible yourself that LilyPond can find it.")),
+            )))
 
 
 class eng_publication(page):
