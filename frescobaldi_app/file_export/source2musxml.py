c7b35206b2625d0bd153d783a5d46d8517a6d1f3
Wilbert Berendsen <wbsoft@xs4all.nl>
2013-10-27 07:34:48 +0100

    use etreeutil.indent to prettyprint xml instead of reparsing using xml.minidom
    also changes tabs to spaces in the python code

--- frescobaldi_app/file_export/source2musxml.py
+++ frescobaldi_app/file_export/source2musxml.py
@@ -37,186 +37,185 @@ from . import ly2xml_mediator
     
 
 class parse_source():
-	""" creates the XML-file from the source code according to the Music XML standard """
-	
-	def __init__(self, doc):
-		self.musxml = create_musicxml.create_musicXML()
-		self.mediator = ly2xml_mediator.mediator()
-		self.prev_command = ''
-		self.varname = ''
-		self.can_create_sect = True
-		self.can_create_part = False
-		self.tuplet = False
-		block = doc.firstBlock()
-		while block.isValid():
-			for t in tokeniter.tokens(block):
-				func_name = t.__class__.__name__ #get instance name
-				if func_name != 'Space':
-					try:
-						func_call = getattr(self, func_name)
-						func_call(t)
-					except AttributeError:
-						# print "Warning: "+func_name+" not implemented!"
-						pass
-			block = block.next()
-		self.mediator.check_score()
-		self.iterate_mediator()
-		
-	def output(self):
-		""" perform line breaks and indents on the string """
-		from xml.dom import minidom		
-		xmlstring = self.musxml.create_xmldoc()
-		return minidom.parseString(xmlstring).toprettyxml(indent="  ")
-		
-	## 
-	# The different source types from ly.lex.lilypond are here sent to translation.
-	##				
-			    	
-	def Name(self, token):
-		""" name of variable """
-		self.varname = token    	
-	
-	def SequentialStart(self, token):
-		""" SequentialStart = { """
-		if self.prev_command[1:] == 'times':
-			self.tuplet = True
-			self.ttype = "start"
-		elif self.prev_command[1:] == 'grace':
-			pass
-		else:
-			if self.can_create_sect:
-				self.mediator.new_section(self.varname)
-				self.can_create_sect = False
-		
-	def SequentialEnd(self, token):
-		""" SequentialEnd = } """
-		if self.tuplet:
-			self.mediator.change_to_tuplet(self.fraction, "stop")
-			self.tuplet = False
-		if self.prev_command:
-			self.prev_command = ''
-		else:
-			self.can_create_sect = True
-			
-	def New(self, token):
-		""" New """
-		self.can_create_part = True
-		
-	def ContextName(self, token):
-		""" staff """
-		if token == "Staff":
-			if self.can_create_part:
-				self.mediator.new_part()
-				self.can_create_sect = False
-				self.can_create_part = False
-			
-	def PipeSymbol(self, token):
-		""" PipeSymbol = | """
-		self.mediator.new_bar()
-		
-	def Clef(self, token):
-		""" Clef \clef"""
-		self.prev_command = "clef"
-		
-	def PitchCommand(self, token):
-		if token == '\relative': # the mode must be absolute
-			pass # not implemented
-		elif token == '\key':
-			self.prev_command = "key"
-					
-	def Note(self, token):
-		""" notename, e.g. c, cis, a bes ... """
-		if self.prev_command == "key":
-			self.key = token
-		else:
-			self.mediator.new_note(token)
-			if self.tuplet:
-				self.mediator.change_to_tuplet(self.fraction, self.ttype)
-				self.ttype = ""
-		
-	def Octave(self, token):
-		""" absolute mode required; a number of , or ' or nothing """
-		self.mediator.new_octave(token)
-		
-	def Length(self, token):
-		""" note length/duration, e.g. 4, 8, 16 ... """
-		self.duration = token
-		self.mediator.new_duration(token)
-		
-	def Dot(self, token):
-		""" dot, . """
-		self.mediator.new_dot()
-		
-	def Tie(self, token):
-		""" tie ~ """
-		self.mediator.tie_to_next()
-		
-	def Rest(self, token):
-		""" rest, r or R. Note: NOT by command, i.e. \rest """	
-		if token == 'R':
-			self.scale = token
-		self.mediator.new_rest(token)
-		
-	def Skip(self, token):
-		""" invisible rest (s) or command \skip """
-		self.mediator.new_rest('s')
-		
-	def Scaling(self, token):
-		""" scaling, e.g. *3 """
-		if self.scale == 'R':
-			self.mediator.scale_rest(token[1:])
-		
-	def Fraction(self, token):
-		""" fraction, e.g. 3/4
-		can be used for time sign or tuplets """
-		if self.prev_command == '\\time':
-			self.mediator.new_time(token)
-			self.prev_command = ''
-		else:
-			self.fraction = token
-		
-	def Keyword(self, token):
-		self.prev_command = token		
-		
-	def Command(self, token):
-		if token == '\\rest':
-			self.mediator.note2rest()
-		else:
-			self.prev_command = token
-		
-	def UserCommand(self, token):
-		if self.prev_command == 'key':
-			self.mediator.new_key(self.key, token)
-			self.prev_command = ''
-		else:
-			self.mediator.fetch_variable(token[1:])
-			print "UserCommand:"+token
-		
-	##
-	# The xml-file is built from the mediator objects
-	##
-			
-	def iterate_mediator(self):
-		""" the mediator lists are looped through and outputed to the xml-file """
-		for part in self.mediator.score:
-			self.musxml.create_part(part.name)
-			self.mediator.set_first_bar(part)
-			for bar in part.barlist:
-				self.musxml.create_measure()
-				for obj in bar:
-					if isinstance(obj, ly2xml_mediator.bar_attr):
-						if obj.has_attr():
-							self.musxml.new_bar_attr(obj.clef, obj.time, obj.key, obj.mode, obj.divs)
-					elif isinstance(obj, ly2xml_mediator.bar_note):
-						self.musxml.new_note([obj.step, obj.alter, obj.octave], obj.duration, obj.type, self.mediator.divisions, obj.dot)
-						if obj.tie:
-							self.musxml.tie_note(obj.tie)
-						if obj.tuplet:
-							self.musxml.tuplet_note(obj.tuplet, obj.duration, obj.ttype, self.mediator.divisions)
-					elif isinstance(obj, ly2xml_mediator.bar_rest):
-						if obj.skip:
-							self.musxml.new_skip(obj.duration, self.mediator.divisions)
-						else:
-							self.musxml.new_rest(obj.duration, obj.type, self.mediator.divisions, obj.pos)
+    """ creates the XML-file from the source code according to the Music XML standard """
+    
+    def __init__(self, doc):
+        self.musxml = create_musicxml.create_musicXML()
+        self.mediator = ly2xml_mediator.mediator()
+        self.prev_command = ''
+        self.varname = ''
+        self.can_create_sect = True
+        self.can_create_part = False
+        self.tuplet = False
+        block = doc.firstBlock()
+        while block.isValid():
+            for t in tokeniter.tokens(block):
+                func_name = t.__class__.__name__ #get instance name
+                if func_name != 'Space':
+                    try:
+                        func_call = getattr(self, func_name)
+                        func_call(t)
+                    except AttributeError:
+                        # print "Warning: "+func_name+" not implemented!"
+                        pass
+            block = block.next()
+        self.mediator.check_score()
+        self.iterate_mediator()
+        self.musxml.indent_xml(indent="  ")
+        
+    def output(self):
+        """ return formatted and indented XML string """
+        return self.musxml.create_xmldoc()
+        
+    ## 
+    # The different source types from ly.lex.lilypond are here sent to translation.
+    ##              
+                    
+    def Name(self, token):
+        """ name of variable """
+        self.varname = token        
+    
+    def SequentialStart(self, token):
+        """ SequentialStart = { """
+        if self.prev_command[1:] == 'times':
+            self.tuplet = True
+            self.ttype = "start"
+        elif self.prev_command[1:] == 'grace':
+            pass
+        else:
+            if self.can_create_sect:
+                self.mediator.new_section(self.varname)
+                self.can_create_sect = False
+        
+    def SequentialEnd(self, token):
+        """ SequentialEnd = } """
+        if self.tuplet:
+            self.mediator.change_to_tuplet(self.fraction, "stop")
+            self.tuplet = False
+        if self.prev_command:
+            self.prev_command = ''
+        else:
+            self.can_create_sect = True
+            
+    def New(self, token):
+        """ New """
+        self.can_create_part = True
+        
+    def ContextName(self, token):
+        """ staff """
+        if token == "Staff":
+            if self.can_create_part:
+                self.mediator.new_part()
+                self.can_create_sect = False
+                self.can_create_part = False
+            
+    def PipeSymbol(self, token):
+        """ PipeSymbol = | """
+        self.mediator.new_bar()
+        
+    def Clef(self, token):
+        """ Clef \clef"""
+        self.prev_command = "clef"
+        
+    def PitchCommand(self, token):
+        if token == '\relative': # the mode must be absolute
+            pass # not implemented
+        elif token == '\key':
+            self.prev_command = "key"
+                    
+    def Note(self, token):
+        """ notename, e.g. c, cis, a bes ... """
+        if self.prev_command == "key":
+            self.key = token
+        else:
+            self.mediator.new_note(token)
+            if self.tuplet:
+                self.mediator.change_to_tuplet(self.fraction, self.ttype)
+                self.ttype = ""
+        
+    def Octave(self, token):
+        """ absolute mode required; a number of , or ' or nothing """
+        self.mediator.new_octave(token)
+        
+    def Length(self, token):
+        """ note length/duration, e.g. 4, 8, 16 ... """
+        self.duration = token
+        self.mediator.new_duration(token)
+        
+    def Dot(self, token):
+        """ dot, . """
+        self.mediator.new_dot()
+        
+    def Tie(self, token):
+        """ tie ~ """
+        self.mediator.tie_to_next()
+        
+    def Rest(self, token):
+        """ rest, r or R. Note: NOT by command, i.e. \rest """  
+        if token == 'R':
+            self.scale = token
+        self.mediator.new_rest(token)
+        
+    def Skip(self, token):
+        """ invisible rest (s) or command \skip """
+        self.mediator.new_rest('s')
+        
+    def Scaling(self, token):
+        """ scaling, e.g. *3 """
+        if self.scale == 'R':
+            self.mediator.scale_rest(token[1:])
+        
+    def Fraction(self, token):
+        """ fraction, e.g. 3/4
+        can be used for time sign or tuplets """
+        if self.prev_command == '\\time':
+            self.mediator.new_time(token)
+            self.prev_command = ''
+        else:
+            self.fraction = token
+        
+    def Keyword(self, token):
+        self.prev_command = token       
+        
+    def Command(self, token):
+        if token == '\\rest':
+            self.mediator.note2rest()
+        else:
+            self.prev_command = token
+        
+    def UserCommand(self, token):
+        if self.prev_command == 'key':
+            self.mediator.new_key(self.key, token)
+            self.prev_command = ''
+        else:
+            self.mediator.fetch_variable(token[1:])
+            print "UserCommand:"+token
+        
+    ##
+    # The xml-file is built from the mediator objects
+    ##
+            
+    def iterate_mediator(self):
+        """ the mediator lists are looped through and outputed to the xml-file """
+        for part in self.mediator.score:
+            self.musxml.create_part(part.name)
+            self.mediator.set_first_bar(part)
+            for bar in part.barlist:
+                self.musxml.create_measure()
+                for obj in bar:
+                    if isinstance(obj, ly2xml_mediator.bar_attr):
+                        if obj.has_attr():
+                            self.musxml.new_bar_attr(obj.clef, obj.time, obj.key, obj.mode, obj.divs)
+                    elif isinstance(obj, ly2xml_mediator.bar_note):
+                        self.musxml.new_note([obj.step, obj.alter, obj.octave], obj.duration, obj.type, self.mediator.divisions, obj.dot)
+                        if obj.tie:
+                            self.musxml.tie_note(obj.tie)
+                        if obj.tuplet:
+                            self.musxml.tuplet_note(obj.tuplet, obj.duration, obj.ttype, self.mediator.divisions)
+                    elif isinstance(obj, ly2xml_mediator.bar_rest):
+                        if obj.skip:
+                            self.musxml.new_skip(obj.duration, self.mediator.divisions)
+                        else:
+                            self.musxml.new_rest(obj.duration, obj.type, self.mediator.divisions, obj.pos)
 
 
