bea289117c488b84988eefa392be1c173b9ff6c4
Wilbert Berendsen <wbsoft@xs4all.nl>
2013-10-30 16:54:12 +0100

    wip simplemarkdown

--- frescobaldi_app/simplemarkdown.py
+++ frescobaldi_app/simplemarkdown.py
@@ -1,19 +1,23 @@
 #!python
 #a very simple markdown-like parser
 
+
+from __future__ import unicode_literals
+
 import re
 
 _line_re = re.compile(
     r"^(?P<blockquote>(\s*>)*)"         # blockquote
     r"(?P<indent>\s*)"                  # initial space (indent)
-    r"((?P<heading>=+)"                 # heading
+    r"(?P<prefix>(?P<heading>=+)"                 # heading
     r"|(?P<verbatim>```(?P<specifier>.*?))\s*$" # verbatim
     r"|(?P<ul>[-*])(?=\s)"              # unordered list item
     r"|(?P<ol>\d+)\.?"                  # ordered list item
     r"|\[(?P<numbered_link>\d+)]"       # numbered link
-        r"\s*(?P<url>\S+)\s*(.*?)\s*$"  # with url and title          
+        r"\s*(?P<url>\S+)\s*(?P<title>.*?)\s*$"  # with url and title          
     r")?\s*(?P<text>.*)$"               # rest of the line
-    )
+    , re.UNICODE)
+
 
 def events(lines):
     """Yield events, parsing the lines."""
@@ -22,20 +26,29 @@ def events(lines):
     blockquote_level = 0
     current_indent = [0]
     
-    for l in lines:
-        if not l or l.isspace():
-            if in_paragraph:
-                in_paragraph = False
-                yield 'PARAGRAPH_END'
-            continue
-        l = l.expandtabs()
+    for line in lines:
+        l = line.expandtabs()
         
         d = _line_re.match(l).groupdict() # will always match
         
+        if in_verbatim:
+            if d['verbatim']:
+                in_verbatim = False
+                yield 'verbatim_end',
+            else:
+                yield 'verbatim_line', line
+            continue
+        
+        if not d['prefix'] and not d['text']:
+            # blank line
+            if in_paragraph:
+                in_paragraph = False
+                yield 'paragraph_end',
+            continue
+            
         # handle numbered inline links
         if d['numbered_link']:
-            yield 'NUMBERED_LINK'
-            yield int(d['numbered_link']), d['url'], d['title']
+            yield 'numbered_link', int(d['numbered_link']), d['url'], d['title']
             continue
         
         # are we in a blockquote?
@@ -43,39 +56,46 @@ def events(lines):
         blockquote_count = d['blockquote'].count('>')
         if blockquote_count == 0 and in_paragraph and blockquote_level > 0:
             pass # keep the blockquote as long as we are in the paragraph
-        elif blockquote_count < blockquote_level:
-            if in_paragraph:
-                yield 'PARAGRAPH_END'
-            for blockquote_level in range(blockquote_level+1, blockquote_count+1):
-                yield 'BLOCKQUOTE_START'
-            if in_paragraph:
-                yield 'PARAGRAPH_START'
         elif blockquote_count > blockquote_level:
             if in_paragraph:
-                yield 'PARAGRAPH_END'
-            for blockquote_level in range(blockquote_level-1, blockquote_count-1, -1):
-                yield 'BLOCKQUOTE_END'
+                yield 'paragraph_end',
+            for blockquote_level in range(blockquote_level+1, blockquote_count+1):
+                yield 'blockquote_start',
             if in_paragraph:
-                yield 'PARAGRAPH_START'
+                yield 'paragraph_start', 
+        elif blockquote_count < blockquote_level:
+            if in_paragraph:
+                yield 'paragraph_end',
+            for blockquote_level in range(blockquote_level-1, blockquote_count-1, -1):
+                yield 'blockquote_end',
+            if in_paragraph:
+                yield 'paragraph_start',
         
         
         # verbatim
         if d['verbatim']:
-            if in_verbatim:
-                in_verbatim = False
-                yield 'VERBATIM_END'
-            else:
-                in_verbatim = True
-                if in_paragraph:
-                    in_paragraph = False
-                    yield 'PARAGRAPH_END'
-                yield 'VERBATIM_START',
-                yield d['specifier']
-        
+            in_verbatim = True
+            if in_paragraph:
+                in_paragraph = False
+                yield 'paragraph_end',
+            yield 'verbatim_start', d['specifier']
+            continue
         
         if not in_paragraph:
             in_paragraph = True
-            yield 'PARAGRAPH_START'
+            yield 'paragraph_start',
 
-        # handle lists and verbatim etc
+        # handle lists
         
+        # finally, handle character level text
+        yield 'text', d['text']
+    
+    # ending
+    if in_verbatim:
+        yield 'verbatim_end',
+    if in_paragraph:
+        yield 'paragraph_end',
+    while blockquote_level > 0:
+        yield 'blockquote_end',
+        blockquote_level -= 1
+
