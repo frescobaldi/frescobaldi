60969429f7577b70f6bfde35fc01296dc4058869
Wilbert Berendsen <wbsoft@xs4all.nl>
2013-11-03 20:54:40 +0100

    in snippet menu call the engraver with the document
    otherwise it could use the "sticky" document if that was set

--- frescobaldi_app/snippet/menu.py
+++ frescobaldi_app/snippet/menu.py
@@ -158,7 +158,7 @@ class TemplateMenu(SnippetMenu):
         from . import snippets
         if 'template-run' in snippets.get(name).variables:
             import engrave
-            engrave.engraver(self.mainwindow()).engravePreview()
+            engrave.engraver(self.mainwindow()).engrave('preview', d)
     
     def clearMenu(self):
         """Deletes the actions on menu hide, except "Manage templates..."."""
