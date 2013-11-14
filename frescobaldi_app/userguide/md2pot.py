75642bc8cbdb48ffaa0113d8a17502455091b8fc
Wilbert Berendsen <wbsoft@xs4all.nl>
2013-11-14 10:17:12 +0100

    charset

--- frescobaldi_app/userguide/md2pot.py
+++ frescobaldi_app/userguide/md2pot.py
@@ -49,7 +49,7 @@ class Parser(userguide.read.Parser):
 msgid ""
 msgstr ""
 "MIME-Version: 1.0\n"
-"Content-Type: text/plain; charset=CHARSET\n"
+"Content-Type: text/plain; charset=UTF-8\n"
 "Content-Transfer-Encoding: utf-8\n"
 "Plural-Forms: nplurals=INTEGER; plural=EXPRESSION;\n"
 
