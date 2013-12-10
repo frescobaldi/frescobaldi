ce8f17a9b7757ae54919ab849efe7ada41ac7f4b
Wilbert Berendsen <wbsoft@xs4all.nl>
2013-12-10 12:15:07 +0100

    add ly.reformat and let reformat use it

--- frescobaldi_app/reformat.py
+++ frescobaldi_app/reformat.py
@@ -23,112 +23,41 @@ Reformat the selection or the whole document, only adjusting whitespace.
 What it does:
 
 - remove trialing whitespace
-
 - newline after all { or << in lilypond mode, unless terminated on same line
 - same way newline before >> or }
+- remove indent for commented lines with more than two comment characters
+- never removes existing newlines
+- Html, scheme, strings, etc are left alone
+
+What it also could do, but not yet:
 
 - newline before and after many non-postfix commands, such as \set, \override
 - at least one blank line between multiline top-level blocks (unless comment)
-
 - wordwrap lines longer than 79 characters
-
-- remove indent for commented lines with more than two comment characters
-
-- Html, scheme, strings, etc are left alone
-- Never removes existing newlines
 
 """
 
 from __future__ import unicode_literals
 
-from PyQt4.QtGui import QTextCursor
-
-import cursortools
-import tokeniter
 import indent
-import ly.lex.lilypond
-import ly.lex.scheme
-
-
-def get_blocks(cursor):
-    """Yields all or the selected blocks."""
-    if cursor.hasSelection():
-        return cursortools.blocks(cursor) 
-    else:
-        return cursortools.all_blocks(cursor.document())
+import lydocument
+import ly.reformat
 
 
 def reformat(cursor):
-    """Reformats the selection or the whole document, adjusting the whitespace."""
-    def newlinebefore(t):
-        pos = block.position() + t.pos
-        d[pos:pos] = '\n'
-    
-    def newlineafter(t):
-        pos = block.position() + t.end
-        d[pos:pos] = '\n'
-    
-    indent_vars = indent.indent_variables(cursor.document())
-    
-    with cursortools.compress_undo(cursor):
-        indent.re_indent(cursor)
-    with cursortools.compress_undo(cursor, True):
-        with cursortools.Writer(cursor.document()) as d:
-            for block in get_blocks(cursor):
-                
-                denters = []
-                tokens = tokeniter.tokens(block)
-                
-                nonspace_index = -1
-                for i, t in enumerate(tokens):
-                    if isinstance(t, ly.lex.Indent) and t in ('{', '<<'):
-                        denters.append(i)
-                    elif isinstance(t, ly.lex.Dedent) and t in ('}', '>>'):
-                        if denters:
-                            denters.pop()
-                        elif nonspace_index != -1:
-                            newlinebefore(t)
-                    elif not isinstance(t, ly.lex.Space):
-                        nonspace_index = i
-                for i in denters:
-                    if i < nonspace_index:
-                        newlineafter(tokens[i])
-                    
-                # TODO: wrap long lines
-        
-    with cursortools.compress_undo(cursor, True):
-        indent.re_indent(cursor)
-        
-    with cursortools.compress_undo(cursor, True):
-        # move commented lines with more than 2 comment characters
-        # to column 0
-        with cursortools.Writer(cursor.document()) as d:
-            for block in get_blocks(cursor):
-                tokens = tokeniter.tokens(block)
-                if (len(tokens) == 2
-                    and isinstance(tokens[0], ly.lex.Space)
-                    and isinstance(tokens[1], (
-                        ly.lex.lilypond.LineComment,
-                        ly.lex.scheme.LineComment))
-                    and len(tokens[1]) > 2
-                    and len(set(tokens[1][:3])) == 1):
-                    del d[block.position():block.position() + tokens[1].pos]
-        
-    with cursortools.compress_undo(cursor, True):
-        remove_trailing_whitespace(cursor)
+    """Reformat the selection or the whole document."""
+    i = indent.indenter(cursor.document())
+    c = lydocument.cursor(cursor)
+    c.document.combine_undo = True
+    ly.reformat.reformat(c, i)
 
 
 def remove_trailing_whitespace(cursor):
-    """Removes whitespace from all lines in the cursor's selection.
+    """Remove trailing whitespace from all lines in the selection.
     
     If there is no selection, the whole document is used.
     
     """
-    with cursortools.Writer(cursor.document()) as d:
-        for block in get_blocks(cursor):
-            length = len(block.text())
-            strippedlength = len(block.text().rstrip())
-            if strippedlength < length:
-                del d[block.position() + strippedlength : block.position() + length]
+    ly.reformat.remove_trailing_whitespace(lydocument.cursor(cursor))
 
 
