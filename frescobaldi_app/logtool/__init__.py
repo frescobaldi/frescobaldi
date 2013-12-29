58c7876aa17221a3aa5f0d4fc32e15481f4db975
Wilbert Berendsen <wbsoft@xs4all.nl>
2013-12-29 13:58:14 +0100

    dont show the log only for auto-compile runs

--- frescobaldi_app/logtool/__init__.py
+++ frescobaldi_app/logtool/__init__.py
@@ -57,13 +57,16 @@ class LogTool(panel.Panel):
     def slotJobStarted(self, doc, job):
         """Called whenever job starts, decides whether to follow it and show the log."""
         import jobattributes
-        if doc == self.mainwindow().currentDocument() or self.mainwindow() == jobattributes.get(job).mainwindow:
+        jattrs = jobattributes.get(job)
+        if doc == self.mainwindow().currentDocument() or self.mainwindow() == jattrs.mainwindow:
             self.widget().switchDocument(doc)
-            if QSettings().value("log/show_on_start", True, bool):
+            if not jattrs.hidden and QSettings().value("log/show_on_start", True, bool):
                 self.show()
 
     def slotJobFinished(self, document, job, success):
+        import jobattributes
         if (not success and not job.isAborted()
+                and not jobattributes.get(job).hidden
                 and document == self.mainwindow().currentDocument()):
             self.show()
     
