021267926ad6bc1402184bc6eae8da8ac623d1e8
Wilbert Berendsen <wbsoft@xs4all.nl>
2014-01-07 23:13:17 +0100

    start work on ly.colorize

---
+++ frescobaldi_app/ly/colorize.py
@@ -0,0 +1,62 @@
+# This file is part of the Frescobaldi project, http://www.frescobaldi.org/
+#
+# Copyright (c) 2013 - 2014 by Wilbert Berendsen
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# See http://www.gnu.org/licenses/ for more information.
+
+"""
+Classes and functions to colorize (syntax-highlight) parsed source.
+"""
+
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+import ly.lex
+
+
+# don't test all the Token base classes
+_token_mro_slice = slice(1, -len(ly.lex.Token.__mro__))
+
+
+class Mapping(dict):
+    """Maps token classes to arbitrary values, which can be highlighting styles.
+    
+    Mapping behaves like a dict, you set items with a token class as key to an
+    arbitrary value.
+    
+    But getting items can be done using a token. The token class's method 
+    resolution order is walked up and the value for the first available 
+    class found in the keys is returned. The class is also cached to speed 
+    up requests for other tokens.
+    
+    """
+    def __getitem__(self, token):
+        cls = type(token)
+        try:
+            return dict.__getitem__(self, cls)
+        except KeyError:
+            for c in cls.__mro__[_token_mro_slice]:
+                try:
+                    value = dict.__getitem__(self, c)
+                    break
+                except KeyError:
+                    pass
+            else:
+                value = None
+            self[cls] = value
+            return value
+
+
