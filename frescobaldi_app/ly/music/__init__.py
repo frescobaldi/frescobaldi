07a46ed9454a75a11cc205cac43ac2998af97d2a
Wilbert Berendsen <wbsoft@xs4all.nl>
2014-01-10 15:43:46 +0100

    start work on ly.music
    this will become the basis under musicxml export and midi play from cursor etc etc

---
+++ frescobaldi_app/ly/music/__init__.py
@@ -0,0 +1,34 @@
+# This file is part of the Frescobaldi project, http://www.frescobaldi.org/
+#
+# Copyright (c) 2014 - 2014 by Wilbert Berendsen
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# See http://www.gnu.org/licenses/ for more information.
+
+"""
+An api to read music from the tokens of a ly.document.Document.
+
+This is meant to quickly read music from a document, to perform 
+modifications on the document, and to interpret music and markup and to 
+convert or export it to other formats.
+
+This package is not intended to construct documents entirely from scratch. 
+(This can be done using the ly.dom module.)
+
+Some item types can have a list of child items, but the tree structure is as 
+linear as possible.
+
+"""
+
---
+++ frescobaldi_app/ly/music/items.py
@@ -0,0 +1,100 @@
+# This file is part of the Frescobaldi project, http://www.frescobaldi.org/
+#
+# Copyright (c) 2014 - 2014 by Wilbert Berendsen
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# See http://www.gnu.org/licenses/ for more information.
+
+"""
+The items a read music expression is constructed with.
+
+Whitespace is left out, but comments are retained.
+
+"""
+
+from __future__ import unicode_literals
+
+
+class Item(object):
+    """Represents any item in the music of a document.
+    
+    This can be just a token, or an interpreted construct such as a note,
+    rest or sequential or simultanuous construct , etc.
+    
+    Some Item instances just have one responsible token, but others have a
+    list or tuple to tokens.
+    
+    An Item also has a pointer to the Document it originates from.
+    
+    """
+    children = ()
+    document = None
+    tokens = ()
+    token = None
+
+
+class Token(Item):
+    """Any token that is not otherwise recognized""" 
+
+
+class Container(Item):
+    """An item having a list of child items."""
+    def __init__(self):
+        self.children = []
+
+
+class Duration(Item):
+    """A duration"""
+    
+    
+class Durable(Item)
+    """An Item that has a Duration attribute."""
+    duration = None
+    
+
+class Chord(Durable, Container):
+    pass
+
+
+class Note(Durable):
+    """A Note that has a ly.pitch.Pitch"""
+    pitch = None
+
+
+class Skip(Durable):
+    pass
+
+
+class Rest(Durable):
+    pass
+
+
+class Music(Container):
+    simultaneous = False
+    """A music expression, either << >> or { }."""
+
+
+class SchemeValue(Item):
+    """The full list of tokens after a #."""
+    
+
+class StringValue(Item):
+    """A double-quoted string."""
+
+
+class Comment(Item):
+    """A comment."""
+    
+
