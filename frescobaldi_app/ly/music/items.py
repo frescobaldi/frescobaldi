e14103f6374928314bf2d5771a25f8a8d069a7e1
Wilbert Berendsen <wbsoft@xs4all.nl>
2014-01-11 15:15:16 +0100

    wip music items

--- frescobaldi_app/ly/music/items.py
+++ frescobaldi_app/ly/music/items.py
@@ -29,6 +29,7 @@ from __future__ import unicode_literals
 
 import node
 
+import ly.pitch
 import ly.lex.lilypond
 
 
@@ -83,6 +84,10 @@ class Rest(Durable):
     pass
 
 
+class Q(Durable):
+    pass
+
+
 class Music(Container):
     simultaneous = False
     """A music expression, either << >> or { }."""
@@ -101,38 +106,102 @@ class Comment(Item):
     
 
 
-def consume(source):
-    """Yield the tokens until a parser is exit."""
-    depth = source.state.depth()
-    for t in source:
-        yield t
-        if source.state.depth() < depth:
-            break
-
-def read(source):
-    """Yield Item instances reading from source."""
-    for t in source:
-        c = t.__class__
-        if c == ly.lex.lilypond.SchemeStart:
-            item = SchemeValue()
-            item.token = t
-            item.tokens = tuple(consume(source))
-            yield item
-        elif issubclass(c, ly.lex.BlockCommentStart):
-            item = Comment()
-            item.token = t
-            item.tokens = tuple(consume(source))
-            yield item
-        elif issubclass(c, ly.lex.Comment):
-            item = Comment()
-            item.token = t
-            yield item
-        elif issubclass(c, ly.lex.StringStart):
-            item = StringValue()
-            item.token = t
-            item.tokens = tuple(consume(source))
-            item.value = ''.join(
-                t[1:] if isinstance(t, ly.lex.Character) and t.startswith('\\') else t
-                for t in item.tokens[:-1])
-            yield item
+class Reader(object):
+    
+    def __init__(self, source):
+        """Initialize with a ly.document.Source.
         
+        The language is set to "nederlands".
+        
+        """
+        self.source = source
+        self.language = "nederlands"
+    
+    def set_language(self, lang):
+        """Changes the pitch name language to use.
+        
+        Called internally when \language or \include tokens are encoutered
+        with a valid language name/file.
+        
+        Sets the language attribute to the language name.
+        
+        """
+        if lang in ly.pitch.pitchInfo:
+            self.language = lang
+            return True
+
+    def consume(self):
+        """Yield the tokens until a parser is exit."""
+        depth = self.source.state.depth()
+        for t in self.source:
+            print 'consuming', t
+            yield t
+            if self.source.state.depth() < depth:
+                break
+
+    def read(self, source=None):
+        """Yield Item instances reading from source."""
+        
+        def factory(cls, token, consume=True):
+            item = cls()
+            item.token = token
+            if consume:
+                item.tokens = tuple(self.consume())
+            return item
+        
+        for t in source or self.source:
+            while isinstance(t, ly.lex.lilypond.MusicItem):
+                item = None
+                if t.__class__ == ly.lex.lilypond.Note:
+                    r = ly.pitch.pitchReader(self.language)(t)
+                    if r:
+                        item = factory(Note, t, False)
+                        p = item.pitch = ly.pitch.Pitch(*r)
+                        t = None # prevent hang in this loop
+                        for t in self.source:
+                            if isinstance(t, ly.lex.lilypond.Octave):
+                                p.octave = ly.pitch.octaveToNum(t)
+                                item.octave_token = t
+                            elif isinstance(t, ly.lex.lilypond.Accidental):
+                                item.accidental_token = p.accidental = t
+                            elif isinstance(t, ly.lex.lilypond.OctaveCheck):
+                                p.octavecheck = ly.pitch.octaveToNum(t)
+                                item.octavecheck_token = t
+                                break
+                            elif not isinstance(t, ly.lex.Space):
+                                break
+                elif t.__class__ == ly.lex.lilypond.Rest:
+                    item = factory(Rest, t, False)
+                    t = None
+                elif t.__class__ in (ly.lex.lilypond.Skip, ly.lex.lilypond.Spacer):
+                    item = factory(Skip, t, False)
+                    t = None
+                else: # t.__class__ == ly.lex.lilypond.Q
+                    item = factory(Q, t, False)
+                    t = None
+                if item:
+                    # wait for a duration
+                    d = item.duration = Duration()
+                    d.tokens = []
+                    if not t or isinstance(t, ly.lex.lilypond.Duration):
+                        if t:
+                            d.tokens.append(t)
+                        for t in self.source:
+                            if isinstance(t, ly.lex.lilypond.Duration):
+                                d.tokens.append(t)
+                            elif not isinstance(t, ly.lex.Space):
+                                break
+                    yield item
+            if isinstance(t, ly.lex.lilypond.SchemeStart):
+                yield factory(SchemeValue, t)
+            elif isinstance(t, ly.lex.BlockCommentStart):
+                yield factory(Comment, t)
+            elif isinstance(t, ly.lex.Comment):
+                yield factory(Comment, t, False)
+            elif isinstance(t, ly.lex.StringStart):
+                item = factory(StringValue, t)
+                item.value = ''.join(
+                    t[1:] if isinstance(t, ly.lex.Character) and t.startswith('\\') else t
+                    for t in item.tokens[:-1])
+                yield item
+            
