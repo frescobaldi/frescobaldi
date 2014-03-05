a3f0a74f05ee3e6212b1cb0ffb624c70239c3836
Wilbert Berendsen <wbsoft@xs4all.nl>
2014-03-05 07:14:47 +0100

    python-ly 0.4

--- frescobaldi_app/ly/pkginfo.py
+++ frescobaldi_app/ly/pkginfo.py
@@ -25,7 +25,7 @@ Information about the LY package.
 from __future__ import unicode_literals
 
 name = "python-ly"
-version = "0.3"
+version = "0.4"
 description = "Tool and library for manipulating LilyPond files"
 long_description = \
     "The python-ly package provides a Python library and a commandline tool " \
