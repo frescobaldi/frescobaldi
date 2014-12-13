6a028b457f5bfea1e752e28aa21de2f8c5480e50
Wilbert Berendsen <wbsoft@xs4all.nl>
2014-12-13 21:15:15 +0100

    use False as default for a boolean value; dont import *

--- frescobaldi_app/midiinput/elements.py
+++ frescobaldi_app/midiinput/elements.py
@@ -2,8 +2,8 @@
 Elements that can bei inserted by MIDI events
 """
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt4.QtCore import Qt
+from PyQt4.QtGui import QApplication
 
 import ly.pitch
 
@@ -19,7 +19,7 @@ class Note:
         self._octave -= 4
         self._pitch = ly.pitch.Pitch(notemapping[self._note][0], notemapping[self._note][1], self._octave)
     
-    def output(self, relativemode=0, language='nederlands'):
+    def output(self, relativemode=False, language='nederlands'):
         if relativemode:
             # makeRelative changes pitch, so we need temporary variables for note and octave
             lastnote = self._pitch.note
@@ -41,7 +41,7 @@ class Chord(object):
     def add(self, note):
         self._notes.append(note)
     
-    def output(self, relativemode=0, language='nederlands'):
+    def output(self, relativemode=False, language='nederlands'):
         if len(self._notes) == 1:    # only one note, no chord
             return self._notes[0].output(relativemode, language)
         else:    # so we have a chord, print <chord>
