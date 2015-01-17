1d06542d0804a69c8d2d42dbeae9c2252e0213c8
Pavel Roskin <proski@gnu.org>
2015-01-16 19:34:33 -0500

    Remove comment about the code in the "six" package
    Our approach to basestring is slightly different, we don't use
    sys.version_info. Suggested by Davide Liessi.

--- macosx/mac-app.py
+++ macosx/mac-app.py
@@ -18,8 +18,6 @@ from subprocess import Popen
 # Python 2 text strings: basestring = str (ASCII) + unicode (Unicode)
 # Python 3 text strings: str (Unicode)
 # See https://docs.python.org/3/howto/pyporting.html for details.
-# The following code is the same used in package six to define a
-# version independent string type for isinstance() tests.
 try:
     string_types = basestring
 except NameError:
