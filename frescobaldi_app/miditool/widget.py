f0f4b6c7d08bdeaf9c010674838c56a10a2aff89
Pavel Roskin <proski@gnu.org>
2015-02-01 03:23:29 -0500

    widget: use explicit imports to avoid redefining class Display

--- frescobaldi_app/miditool/widget.py
+++ frescobaldi_app/miditool/widget.py
@@ -23,8 +23,9 @@ The MIDI tool widget.
 
 from __future__ import unicode_literals
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt4.QtCore import Qt, QTimer, QSettings
+from PyQt4.QtGui import (
+    QWidget, QComboBox, QToolButton, QSlider, QGridLayout, QSizePolicy, QLabel)
 
 import app
 import css
