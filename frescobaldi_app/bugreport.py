8670c448c7db86f1b646367b2d99d4baca41b0dd
Wilbert Berendsen <wbsoft@xs4all.nl>
2015-05-19 15:24:19 +0200

    use debuginfo in bugreport module

--- frescobaldi_app/bugreport.py
+++ frescobaldi_app/bugreport.py
@@ -27,72 +27,13 @@ from PyQt4.QtCore import QUrl
 
 import helpers
 import appinfo
+import debuginfo
 
-
-def versionInfo():
-    """Returns version and platform information as a dict for debugging purposes."""
-    try:
-        import sip
-        sip_version = sip.SIP_VERSION_STR
-    except (ImportError, NameError):
-        sip_version = "unknown"
-    
-    try:
-        import PyQt4.QtCore
-        pyqt_version = PyQt4.QtCore.PYQT_VERSION_STR
-    except (ImportError, NameError):
-        pyqt_version = "unknown"
-    
-    try:
-        qt_version = PyQt4.QtCore.QT_VERSION_STR
-    except NameError:
-        qt_version = "unknown"
-    
-    try:
-        import platform
-        python_version = platform.python_version()
-        osname = platform.platform()
-    except (ImportError, NameError):
-        python_version = "unknown"
-        osname = "unknown"
-    
-    try:
-        import ly.pkginfo
-        ly_version = ly.pkginfo.version
-    except (ImportError, NameError):
-        ly_version = "unknown"
-    
-    try:
-        import popplerqt4
-        poppler_version = '.'.join(str(n) for n in popplerqt4.poppler_version())
-        python_poppler_version = '.'.join(str(n) for n in popplerqt4.version())
-    except ImportError:
-        poppler_version = "unknown"
-        python_poppler_version = "missing"
-    except AttributeError:
-        poppler_version = "unknown"
-        python_poppler_version = "unknown"
-
-    return locals()
-
-
-def versionInfoString():
-    """Returns the information from versionInfo() formatted as a terse string."""
-    return (
-        "Python: {python_version} -- "
-        "Qt: {qt_version} -- "
-        "PyQt4: {pyqt_version} -- "
-        "sip: {sip_version}\n"
-        "python-ly: {ly_version}\n"
-        "poppler: {poppler_version} -- "
-        "python-poppler-qt4: {python_poppler_version}\n"
-        "OS: {osname}".format(**versionInfo()))
-    
 
 def email(subject, body):
     """Opens the e-mail composer with the given subject and body, with version information added to it."""
     subject = "[{0} {1}] {2}".format(appinfo.appname, appinfo.version, subject)
-    body = "{0}: {1}\n\n{2}\n\n{3}\n\n".format(appinfo.appname, appinfo.version, versionInfoString(), body)
+    body = "{0}\n\n{1}\n\n".format(debuginfo.version_info_string(' -- '), body)
     url = QUrl("mailto:" + appinfo.maintainer_email)
     url.addQueryItem("subject", subject)
     url.addQueryItem("body", body)
