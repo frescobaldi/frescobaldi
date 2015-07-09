486b3f32d5bd916ebe08458e60ae112440e622d5
Urs Liska <git@ursliska.de>
2015-07-09 11:02:25 +0200

    popplerwidget: Remove whitespace

--- frescobaldi_app/viewers/popplerwidget.py
+++ frescobaldi_app/viewers/popplerwidget.py
@@ -53,28 +53,28 @@ from . import pointandclick
 
 class PopplerView(QWidget):
     """Widget containing the qpopplerview.View."""
-    
+
     zoomChanged = pyqtSignal(int, float) # mode, scale
-    
+
     def __init__(self, dockwidget):
         """Creates the Music View for the dockwidget."""
         super(PopplerView, self).__init__(dockwidget)
-        
+
         self._positions = weakref.WeakKeyDictionary()
         self._currentDocument = None
         self._links = None
         self._clicking_link = False
-        
+
         self._highlightFormat = QTextCharFormat()
         self._highlightMusicFormat = Highlighter()
         self._highlightRange = None
         self._highlightTimer = QTimer(singleShot=True, interval= 250, timeout=self.updateHighlighting)
         self._highlightRemoveTimer = QTimer(singleShot=True, timeout=self.clearHighlighting)
-        
+
         layout = QVBoxLayout()
         layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(layout)
-        
+
         self.view = popplerview.View(self)
         layout.addWidget(self.view)
         app.settingsChanged.connect(self.readSettings)
@@ -86,26 +86,26 @@ class PopplerView(QWidget):
         self.view.surface().linkLeft.connect(self.slotLinkLeft)
         self.view.surface().setShowUrlTips(False)
         self.view.surface().linkHelpRequested.connect(self.slotLinkHelpRequested)
-        
+
         self.view.viewModeChanged.connect(self.updateZoomInfo)
         self.view.surface().pageLayout().scaleChanged.connect(self.updateZoomInfo)
         self.view.setContextMenuPolicy(Qt.CustomContextMenu)
         self.view.customContextMenuRequested.connect(self.showContextMenu)
-        
+
         # react if cursor of current text document moves
         dockwidget.mainwindow().currentViewChanged.connect(self.slotCurrentViewChanged)
         view = dockwidget.mainwindow().currentView()
         if view:
             self.slotCurrentViewChanged(view)
-    
+
     def sizeHint(self):
         """Returns the initial size the PDF (Music) View prefers."""
         return self.parent().mainwindow().size() / 2
-    
+
     def updateZoomInfo(self):
         """Called when zoom and viewmode of the qpopplerview change, emit zoomChanged."""
         self.zoomChanged.emit(self.view.viewMode(), self.view.surface().pageLayout().scale())
-    
+
     def openDocument(self, doc):
         """Opens a documents.Document instance."""
         self.clear()
@@ -127,7 +127,7 @@ class PopplerView(QWidget):
         self._highlightRange = None
         self._highlightTimer.stop()
         self.view.clear()
-        
+
     def readSettings(self):
         """Reads the settings from the user's preferences."""
         # background and highlight colors of music view
@@ -139,10 +139,10 @@ class PopplerView(QWidget):
 
     def slotLinkClicked(self, ev, page, link):
         """Called when the use clicks a link.
-        
+
         If the links is a textedit link, opens the document and puts the cursor there.
         Otherwise, call the helpers module to open the destination.
-        
+
         """
         if ev.button() == Qt.RightButton:
             return
@@ -166,13 +166,13 @@ class PopplerView(QWidget):
 
     def slotLinkHovered(self, page, link):
         """Called when the mouse hovers a link.
-        
+
         If the links points to the current editor document, the token(s) it points
         at are highlighted using a transparent selection color.
-        
+
         The highlight shows for a few seconds but disappears when the mouse moves
         off the link or when the link is clicked.
-        
+
         """
         self.view.surface().highlight(self._highlightMusicFormat,
             [(page, link.linkArea().normalized())], 2000)
@@ -180,13 +180,13 @@ class PopplerView(QWidget):
         cursor = self._links.cursor(link)
         if not cursor or cursor.document() != self.parent().mainwindow().currentDocument():
             return
-        
+
         # highlight token(s) at this cursor
         cursors = pointandclick.positions(cursor)
         if cursors:
             view = self.parent().mainwindow().currentView()
             viewhighlighter.highlighter(view).highlight(self._highlightFormat, cursors, 2, 5000)
-    
+
     def slotLinkLeft(self):
         """Called when the mouse moves off a previously highlighted link."""
         self.clearHighlighting()
@@ -213,17 +213,17 @@ class PopplerView(QWidget):
         if old:
             old.cursorPositionChanged.disconnect(self.slotCursorPositionChanged)
         view.cursorPositionChanged.connect(self.slotCursorPositionChanged)
-    
+
     def slotCursorPositionChanged(self):
         """Called when the user moves the text cursor."""
         if not self.isVisible() or not self._links:
             return # not visible of no PDF in the viewer
-        
+
         view = self.parent().mainwindow().currentView()
         links = self._links.boundLinks(view.document())
         if not links:
             return # the PDF contains no references to the current text document
-        
+
         s = links.indices(view.textCursor())
         if s is False:
             self.clearHighlighting()
@@ -237,7 +237,7 @@ class PopplerView(QWidget):
                 self.view.ensureVisible(center.x(), center.y(),
                                         50 + rect.width() // 2,
                                         50 + rect.height() // 2)
-            
+
             # perform highlighting after move has been started. This is to ensure that if kinetic scrolling is
             # is enabled its speed is already set so that we can adjust the highlight timer.
             self.highlight(links.destinations(), s)
@@ -250,7 +250,7 @@ class PopplerView(QWidget):
             # RC: increased timer to give some time to the kinetic scrolling to complete.
             kineticTimeLeft = 0
             if self.view.kineticScrollingEnabled():
-                kineticTimeLeft = 20*self.view.kineticTicksLeft() 
+                kineticTimeLeft = 20*self.view.kineticTicksLeft()
             msec = 5000 if count > 1 else 2000 # show selections longer
             msec += kineticTimeLeft
         self._highlightRemoveTimer.start(msec)
@@ -263,7 +263,7 @@ class PopplerView(QWidget):
         else:
             self._highlightTimer.stop()
             self.updateHighlighting()
-    
+
     def updateHighlighting(self):
         """Really orders the view's surface to draw the highlighting."""
         layout = self.view.surface().pageLayout()
@@ -271,7 +271,7 @@ class PopplerView(QWidget):
                     for dest in self._destinations
                     for pageNum, rect in dest]
         self.view.surface().highlight(self._highlightMusicFormat, areas)
-    
+
     def clearHighlighting(self):
         """Called on timeout of the _highlightRemoveTimer."""
         self._highlightRange = None
@@ -281,18 +281,18 @@ class PopplerView(QWidget):
         """Scrolls the view if necessary to show objects at current text cursor."""
         if not self._links:
             return # no PDF in viewer
-            
+
         view = self.parent().mainwindow().currentView()
         links = self._links.boundLinks(view.document())
         if not links:
             return # the PDF contains no references to the current text document
-        
+
         s = links.indices(view.textCursor())
         if not s:
             return
         self.view.center(self.destinationsRect(links.destinations()[s]).center())
         self.highlight(links.destinations(), s, 10000)
-    
+
     def destinationsRect(self, destinations):
         """Return the rectangle containing all destinations."""
         layout = self.view.surface().pageLayout()
@@ -303,7 +303,7 @@ class PopplerView(QWidget):
         # not larger than viewport
         rect.setSize(rect.size().boundedTo(self.view.viewport().size()))
         return rect
-    
+
     def showContextMenu(self):
         """Called when the user right-clicks or presses the context menu key."""
         pos = self.view.mapToGlobal(QPoint(0, 0))
@@ -321,16 +321,14 @@ class PopplerView(QWidget):
 
 class Highlighter(qpopplerview.Highlighter):
     """Simple version of qpopplerview.Highlighter that has the color settable.
-    
+
     You must set a color before using the Highlighter.
-    
+
     """
     def setColor(self, color):
         """Sets the color to use to draw highlighting rectangles."""
         self._color = color
-    
+
     def color(self):
         """Returns the color set using the setColor method."""
         return self._color
-
-
