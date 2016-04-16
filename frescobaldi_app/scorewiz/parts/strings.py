18c20a2c394951ab3f414a992be2aa59b44411d3
Wilbert Berendsen <wbsoft@xs4all.nl>
2016-04-16 21:26:58 +0200

    fix #808 \figuremode should be enclosed within \new FiguredBass

--- frescobaldi_app/scorewiz/parts/strings.py
+++ frescobaldi_app/scorewiz/parts/strings.py
@@ -106,9 +106,9 @@ class BassoContinuo(Cello):
                     "#'direction = #DOWN", b)
         ly.dom.LineComment(_("Figures follow here."), b)
         ly.dom.BlankLine(b)
-        s = ly.dom.Sim(data.nodes[0][-1])
-        s.append(data.nodes[0][-1][-2])
-        ly.dom.Identifier(a.name, s)
+        figbass = ly.dom.FiguredBass()
+        ly.dom.Identifier(a.name, figbass)
+        data.nodes.append(figbass)
     
 
 register(
