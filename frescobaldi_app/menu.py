86ea387bee2024bec73c14066b38bd06cfb1939d
Wilbert Berendsen <wbsoft@xs4all.nl>
2016-04-19 11:49:33 +0200

    menu tooltips visible on qt5

--- frescobaldi_app/menu.py
+++ frescobaldi_app/menu.py
@@ -83,6 +83,7 @@ class Menu(QMenu):
     def __init__(self, title_func, parent=None):
         """title_func should return the title for the menu when called."""
         super(Menu, self).__init__(parent)
+        self.setToolTipsVisible(True)
         self.title_func = title_func
         app.translateUI(self)
     
