4160ef55ade7f71ffc18b50754eac0820b90a4d4
Wen Xin <wx@whensean.com>
2017-06-08 22:07:57 +0800

    Add the draft of new 'git Repo' Only a feature-list is contained by far.

---
+++ frescobaldi_app/vcs/gitrepoo.py
@@ -0,0 +1,34 @@
+class Repo():
+	"""
+	"""
+	############################################################################
+	# features
+	# 1.  whether the working directory is a git repo
+	# 2.  extract HEAD, Index and working file and save them to tmp files
+	# 3.  update HEAD, Index, working temp files
+	# 4.  run git diff command between temp files and get output
+	# 5.  parse git diff output
+	# 6.  current_branch() 
+	# 7.  branches()
+    # 8.  checkout()
+    # 9.  has_branch()
+    # 10. has_remote() 
+	# 11. has_remote_branch()
+	# 12. remotes()
+	# 13. tracked_remote()
+	# 14. tracked_remote_label()
+	# 15. set_working_directory()
+	# 16. whether the file is an untracked file
+	# 17. whether the file is an ignored file
+	# 18. whether git has been installed 
+	# 19. use which git
+	# 19. get git version
+	############################################################################
+	def branches(self):
+		pass
+	def current_branch(self):
+		pass
+	def has_branch(self)
+	    pass
+	def has_remote(self)
+	    pass
