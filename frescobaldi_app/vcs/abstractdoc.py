3b9c6a116d05488b13b504717d4eaeb686bffbd5
Urs Liska <git@ursliska.de>
2017-11-05 16:10:35 +0100

    vcs.abstractdoc: Reorder functions
    Sort functions by
    - special functions
    - class methods
    - abstract methods
    - implemented methods

--- frescobaldi_app/vcs/abstractdoc.py
+++ frescobaldi_app/vcs/abstractdoc.py
@@ -28,6 +28,11 @@ import tempfile
 from PyQt5.QtCore import QObject, QUrl, pyqtSignal
 
 class Document(QObject):
+    """
+    Abstract base class representing a VCS controlled document.
+    This 'document' does not contain the content of a document
+    but only its VCS status and metadata.
+    """
 
     __metaclass__ = ABCMeta
 
@@ -36,25 +41,6 @@ class Document(QObject):
 
     _queue_class = None
     
-    @classmethod
-    def _create_tmp_file(cls, dir = None, prefix = 'Frescobaldi_vcs_'):
-        """Creates a new tmp file and return the path to it.
-
-        CAUTION: Caller is responsible for clean up
-
-        Args:
-            dir(string): If dir is specified, the file will be created in that
-                directory, otherwise a default directory is used.
-            prefix(string): the prefix of new created tmp file
-
-        Returns:
-            Return a absolute path to the new tmp file.
-
-        """
-        file, filepath = tempfile.mkstemp(prefix = prefix, dir = dir)
-        os.close(file)
-        return filepath
-
     def __init__(self, root_path, relative_path, view):
         super(Document, self).__init__()
         self._view = view
@@ -75,46 +61,79 @@ class Document(QObject):
         self._jobqueue.kill_all()
         self._clean_up_temp_files()
     
-    @abstractmethod
-    def _create_tmp_files(self):
-        pass
+    @classmethod
+    def _create_tmp_file(cls, dir = None, prefix = 'Frescobaldi_vcs_'):
+        """Creates a new tmp file and return the path to it.
 
+        CAUTION: Caller is responsible for clean up
 
-    @abstractmethod
-    def _clean_up_temp_files(self):
-        pass
+        Args:
+            dir(string): If dir is specified, the file will be created in that
+                directory, otherwise a default directory is used.
+            prefix(string): the prefix of new created tmp file
+
+        Returns:
+            Return a absolute path to the new tmp file.
+
+        """
+        file, filepath = tempfile.mkstemp(prefix = prefix, dir = dir)
+        os.close(file)
+        return filepath
 
     @classmethod
     def _write_file(cls, path, content):
         """
         Write the content into the path's corresponding file.
+        Used for the temporary files.
         """
         with open(path, 'wb') as file:
             file.write(content)
             file.flush()
             os.fsync(file.fileno())
 
+    @abstractmethod
+    def _update_diff_lines(repoChanged, fileChanged):
+        pass
+    
+    @abstractmethod
+    def _update_status(self):
+        pass
+    
+    @abstractmethod
+    def _create_tmp_files(self):
+        """
+        Create the (VCS dependent) temporary files to handle the
+        different states of the document.
+        """
+        pass
+
+    @abstractmethod
+    def _clean_up_temp_files(self):
+        """
+        Cleanly delete the temporary files created upon initialization.
+        """
+        pass
+
+    @abstractmethod
+    def diff_hunk(self, row):
+        """Get specific diff hunk
+
+        Args:
+            row(int): a line number.
+
+        Returns:
+            A dictionary.
+
+        """
+        pass
+
+    @abstractmethod
+    def is_tracked(self):
+        pass
+
     def _file_changed_update(self):
         self.update(fileChanged = True)
 
-    def view(self):
-        return self._view
-
-    def status(self):
-        return self._status
-
-    def root(self):
-        return self._root_path
-    
-    def relative_path(self):
-        return self._relative_path
-
-    def path(self):
-        return self._path
-    
-    def url(self):
-        return QUrl(self._path)
-    
     def diff_lines(self):
         """Get current file's line-diff result
 
@@ -133,31 +152,6 @@ class Document(QObject):
         return self._diff_lines
 
 
-    @abstractmethod
-    def diff_hunk(self, row):
-        """Get specific diff hunk
-
-        Args:
-            row(int): a line number.
-
-        Returns:
-            A dictionary.
-
-        """
-        pass
-
-    @abstractmethod
-    def _update_status(self):
-        pass
-    
-    @abstractmethod
-    def _update_diff_lines(repoChanged, fileChanged):
-        pass
-    
-    @abstractmethod
-    def is_tracked(self):
-        pass
-
     def update(self, repoChanged = False, fileChanged = False):
         if self.is_tracked():
             # TODO: implement a delay (e.g. 100-500 ms) like with automatic compilation
@@ -173,3 +167,22 @@ class Document(QObject):
         except Exception: pass
         self._clean_job()
 
+    # Properties
+
+    def view(self):
+        return self._view
+
+    def status(self):
+        return self._status
+
+    def root(self):
+        return self._root_path
+    
+    def relative_path(self):
+        return self._relative_path
+
+    def path(self):
+        return self._path
+    
+    def url(self):
+        return QUrl(self._path)
