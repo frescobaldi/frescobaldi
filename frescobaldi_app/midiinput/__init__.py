4aed113a2751018e487d0f40d30a5a0ba9749e34
Dmitriy <d.klochan@gmail.com>
2017-11-19 17:33:39 +0300

    Adding a "replace mode" to MIDI Input #866
    Adding a "replace mode" to MIDI Input #866

--- frescobaldi_app/midiinput/__init__.py
+++ frescobaldi_app/midiinput/__init__.py
@@ -113,16 +113,19 @@ class MidiIn(object):
         doc = cursor.document()
 
         if self.widget().repitchmode():
-              rithm_str = doc.toPlainText()[cursor.position() : doc.characterCount()]
               # Regular expressions that match lilypond music string
-              delim = '[\s\d\.\(\)\{\}\~]' # preceding symbols we don't want overwrite
+              delim = '[\s\d\.\~\(\)\{\}\[\]\|]' # preceding symbols we don't want overwrite
               chord_ptrn = '<[^>^]*>' # lilypond chord pattern
               notes_ptrn = '[a-ps-zA-PS-Z]+[\'\,]*' # lilypond note pattern excludes "r" and "R" symbols to leave rests untouched
               pitch_ptrn = delim + chord_ptrn +'|'+ delim + notes_ptrn 
+
+              # get the remaining document content and match pattern
+              # insert the space to allow matching from very beginning (if happen no preceding symbols)
+              rithm_str = ' ' + doc.toPlainText()[cursor.position() : doc.characterCount()]
               notes = re.search(pitch_ptrn,rithm_str)
               if notes != None :
-                    start = cursor.position() + notes.start() + 1 # +1 for keeping unchanged the first found character
-                    end = cursor.position() + notes.end()
+                    start = cursor.position() + notes.start()
+                    end = cursor.position() + notes.end() - 1 # -1 to compensate the space insertion
                     cursor.setPosition(start, QTextCursor.MoveAnchor)
                     cursor.setPosition(end, QTextCursor.KeepAnchor)
                     cursor.insertText(text)
