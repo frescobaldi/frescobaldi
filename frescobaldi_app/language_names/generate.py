frescobaldi/frescobaldi

commit add5af108eb9e794479113922edea99d12ffcc19

Remove all __future__ imports
Closes #1003

It seems these are all obsolete by now since Python2 isn't supported anymore.

frescobaldi_app/language_names/generate.py
@@ -89,7 +89,7 @@ def write_dict(langs):
     with codecs.open("data.py", "w", "utf-8") as output:
         output.write("# -*- coding: utf-8;\n\n")
         output.write("# Do not edit, this file is generated. See generate.py.\n")
-        output.write("\nfrom __future__ import unicode_literals\n")
+        output.write("\n
         output.write("\n\n")
 
         output.write("language_names = {\n")

frescobaldi_app/app.py
@@ -21,7 +21,7 @@
 The global things in Frescobaldi.
 """
 
-from __future__ import print_function
+
 
 import os
 import sys

frescobaldi_app/debug.py
@@ -6,7 +6,7 @@
 # signals to debug-print functions, and imports the most important modules such
 # as app.
 
-from __future__ import print_function
+
 
 import sys
 

frescobaldi_app/hyphenator.py
@@ -14,7 +14,7 @@ info@wilbertberendsen.nl
 License: LGPL. More info: http://python-hyphenator.googlecode.com/
 
 """
-from __future__ import print_function
+
 
 try:
     chr = unichr

frescobaldi_app/lydocinfo.py
@@ -28,7 +28,7 @@ available to both text documents on disk and loaded Frescobaldi documents.
 
 """
 
-from __future__ import absolute_import
+
 
 import re
 

frescobaldi_app/lydocument.py
@@ -31,7 +31,7 @@ discard it.
 
 """
 
-from __future__ import absolute_import
+
 
 from PyQt5.QtGui import QTextCursor
 

frescobaldi_app/mainwindow.py
@@ -21,7 +21,7 @@
 Frescobaldi Main Window.
 """
 
-from __future__ import division
+
 
 import itertools
 import os

frescobaldi_app/midifile/parser.py
@@ -29,7 +29,7 @@ Runs with Python 2.6, 2.7 and 3.
 
 """
 
-from __future__ import print_function
+
 
 import sys
 import struct

commit 65f60e6f4b9ce3c05009609fced194bef3c3631c

Fix failed code removal __futures__
The error was introduced in add5af108eb9e794479113922edea99d12ffcc19

frescobaldi_app/language_names/generate.py
@@ -89,7 +89,6 @@ def write_dict(langs):
     with codecs.open("data.py", "w", "utf-8") as output:
         output.write("# -*- coding: utf-8;\n\n")
         output.write("# Do not edit, this file is generated. See generate.py.\n")
-        output.write("\n
         output.write("\n\n")
 
         output.write("language_names = {\n")
@@ -105,4 +104,3 @@ if __name__ == "__main__":
     langs = generate_kde()
     langs['zh'] = langs['zh_CN']
     write_dict(langs)
-