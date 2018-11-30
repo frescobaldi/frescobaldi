e49f2c8362f7c267785d46a4122d7e9d519dbe34
Urs Liska <git@ursliska.de>
2018-11-30 16:04:56 +0100

    Adjust VBCL to Frescobaldi's needs
    Originally the VBCL module had been designed specifically for openLilyLib
    packages. In Frescobaldi it has to become somewhat more generic in order
    to become useful for future generic use. For now we need it to handle
    extension metadata, but it will also be available for other extensions
    (for example openLilyLib, again).
    
    Therefore, the data against to check must not be built into the module
    but passed from outside (here we'll do it from the extensions package).

--- frescobaldi_app/vbcl/__init__.py
+++ frescobaldi_app/vbcl/__init__.py
@@ -1,7 +1,24 @@
-#!/usr/bin/env python3
+# This file is part of the Frescobaldi Extensions project,
+# https://github.com/frescobaldi-extensions
 #
+# Copyright (c) 2018 by Urs Liska and others
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# See http://www.gnu.org/licenses/ for more information.
+
 # VBCL parser
-#
 # Andrew Bernard 2017
 
 import re
@@ -14,29 +31,22 @@ long_text_end = re.compile(r"^\s*>")
 list_items_start = re.compile(r"^(.*):\s+\[")
 list_items_end = re.compile(r"^\s*\]")
 
-defaults = {
-    'name': 'NN',
-    'display-name': 'NN',
-    'short-description': None,
-    'description': None,
-    'website': None,
-    'repository': None,
-    'dependencies': [],
-    'oll-core': '0.0.0',
-    'maintainers': [],
-    'version': '0.0.0',
-    'license': 'None'
-}
 
+def check_mandatory_keys(d, mandatory_keys):
+    """Check if all mandatory keys are present. Raise a ValueError if not."""
+    if mandatory_keys:
+        for key in mandatory_keys:
+            if not key in d.keys():
+                raise ValueError(
+                _("VBCL Error: Missing mandatory key '{}'".format(key)))
 
-def set_defaults(d):
-    """Ensure mandatory properties are set to 'empty' values."""
+def set_defaults(d, defaults):
+    """Ensure optional properties are set to default/'empty' values."""
     for key in defaults:
         d[key] = d.get(key, defaults[key])
-    return d
 
 
-def parse(lines):
+def parse(lines, mandatory_keys, defaults):
     """Returns a dictionary corresponding to a parsed VBCL string list."""
     d = dict()
     it = iter(lines)
@@ -80,13 +90,15 @@ def parse(lines):
     except StopIteration:
         pass
 
-    cfg = set_defaults(d)
-    return cfg
+    check_mandatory_keys(d, mandatory_keys)
+    set_defaults(d, defaults)
+    return d
 
 
-def parse_file(filename):
-    """Returns a dictionary corresponding to a parsed VBCL config file."""
+def parse_file(filename, mandatory_keys=None, defaults=None):
+    """Returns a dictionary corresponding to a parsed VBCL config file.
+    Raises an exception if the given file doesn't exist or isn't readable"""
 
     with open(filename) as f:
-        cfg_dict = parse(f.read().split('\n'))
+        cfg_dict = parse(f.read().split('\n'), mandatory_keys, defaults)
         return cfg_dict
