aca601215485377d2e39497eff64bad4bb816105
Wilbert Berendsen <wbsoft@xs4all.nl>
2019-08-11 00:51:11 +0200

    doc

--- frescobaldi_app/qpageview/pagedview.py
+++ frescobaldi_app/qpageview/pagedview.py
@@ -1,6 +1,6 @@
 # This file is part of the qpageview package.
 #
-# Copyright (c) 2016 - 2019 by Wilbert Berendsen
+# Copyright (c) 2019 - 2019 by Wilbert Berendsen
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -18,7 +18,7 @@
 # See http://www.gnu.org/licenses/ for more information.
 
 """
-The View, deriving from QAbstractScrollArea.
+Mixin class to add paging capabilities to View.
 """
 
 from PyQt5.QtCore import pyqtSignal
