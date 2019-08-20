d11fdf69f77cf17ab974c201175d091bd875ed4d
Wilbert Berendsen <wbsoft@xs4all.nl>
2019-08-20 14:34:40 +0200

    lots of work on composite. paint on pixmaps, working on image()

--- frescobaldi_app/qpageview/composite.py
+++ frescobaldi_app/qpageview/composite.py
@@ -25,6 +25,11 @@ the pages are done in the renderer.
 
 """
 
+import types
+import weakref
+
+from PyQt5.QtCore import QPoint
+from PyQt5.QtGui import QPainter, QPixmap
 
 from . import page
 from . import render
@@ -34,30 +39,202 @@ from . import render
 class CompositePage(page.AbstractPage):
     """A Page that can draw multiple pages over each other.
     
-    The pages to be drawn are in the pages attribute.
+    The page that is painted first is put in the `base` attribute on
+    instantiation, the other pages are appended to the `overlay` list.
+    
+    The base page dictates the dimensions and natural size of the
+    CompositePage. The overlay pages are painted on top of the first page and
+    positioned and scaled using the CompositePage.fit() method.
+    
+    Don't replace the overlay list later, but just change it; this makes
+    sure that copied pages (e.g. the SidebarView or the Magnifier) use the
+    correct information.
+    
+    A CompositePage has a renderer, but this one delegates the rendering
+    to the "sub"pages, and just calls the paint() method of the sub pages. How
+    those images are combined is up to the renderer.
+    
+    You should not use a sub Page from a CompositePage independently, as the
+    CompositeRenderer modifies the position and dimensions. When you also want
+    to use the original Page object, make a copy first.
     
     """
-    def __init__(self, pages=None, renderer=None):
+    def __init__(self, page, renderer=None):
         super().__init__()
-        self.pages = pages if pages is not None else []
-        self.pageWidth = max(p.pageWidth for p in self.pages)
-        self.pageHeight = max(p.pageHeight for p in self.pages)
+        self.base = page
+        self.overlay = []
+        self.pageWidth = page.pageWidth
+        self.pageHeight = page.pageHeight
         if renderer is not None:
             self.renderer = renderer
+    
+    def fitbase(self):
+        """Make sure the base page has the same size as ourselves."""
+        # base.x and y are not used
+        self.base.width = self.width
+        self.base.height = self.height
+        self.base.computedRotation = self.computedRotation
+        
+    def fitoverlay(self, page):
+        """Fits the overlay page in our page.
+        
+        By default this method centers the other page and scales it up
+        as large as possible. The x and y coordinates of the overlay pages
+        are used to specify their position relative to the base page.
+        
+        """
+        pageWidth = page.pageWidth * page.scaleX
+        pageHeight = page.pageHeight * page.scaleY
+        if self.computedRotation & 1:
+            pageWidth, pageHeight = pageHeight, pageWidth
+
+        scaleX = self.width / pageWidth
+        scaleY = self.height / pageHeight
+        scale = min(scaleX, scaleY)
+        page.width = pageWidth * scale
+        page.height = pageHeight * scale
+        page.x = (self.width - page.width) // 2
+        page.y = (self.height - page.height) // 2
+        page.computedRotation = self.computedRotation
+    
+    def fitpages(self):
+        """Calls fitbase() and fitoverlay() for all overlay pages."""
+        self.fitbase()
+        for p in self.overlay:
+            self.fitoverlay(p)
+
+    def image(self, rect, dpiX=72.0, dpiY=None):
+        """Return a QImage of the specified rectangle, of all images combined."""
+        self.fitpages()
+        image = self.base.image(rect, dpiX, dpiY)
+        painter = QPainter(image)
+        for layer, p in enumerate(self.overlay):
+            r = rect.translated(-p.pos()) & p.rect()
+            painter.save()
+            painter.setOpacity(self.renderer.opacity[layer])
+            # TODO: compute the correct resolution
+            # which scale was used by fitoverlay()? (which may have been reimplemented)
+            painter.drawImage(r.topLeft()-rect.topLeft(), p.image(r, dpiX, dpiY))
+            painter.restore()
+        painter.end()
+        return image
+        
+    def text(self, rect):
+        """Reimplemented to return the text from the base Page."""
+        self.fitbase() # make sure the rect is understood correctly
+        return self.base.text(rect)
+    
+    def links(self):
+        """Reimplemented to return the links from the base Page."""
+        return self.base.links()
 
 
-class Renderer(render.AbstractImageRenderer):
+class CompositeRenderer(render.AbstractImageRenderer):
     """Paints the pages by calling their own renderer."""
     
     def __init__(self):
         super().__init__()
-        self.opacity = [1.0, 0.5]
+        self._callbacks = weakref.WeakKeyDictionary()
+        self.opacity = [0.5]
     
+    def makecallback(self, callback, page):
+        """Return a callback for the composite page, the same if possible.
+        
+        This callback is called when rendering a base or overlay page is
+        finished, and in turn it calls the original callback with the original
+        composite page.
+        
+        We cache the callback using a weak reference; this makes sure that
+        unscheduling rendering jobs works correctly.
+        
+        """
+        # household: first remove dead refs from all page dicts
+        deadcallbacks = [(r, page)
+                            for page, d in self._callbacks.items()
+                                for r in d if r() is None]
+        for r, page in deadcallbacks:
+            del self._callbacks[page][r]
+            if not self._callbacks[page]:
+                del self._callbacks[page]
+        
+        # make a weak ref to the callback
+        if type(callback) is types.MethodType:
+            callbackref = weakref.WeakMethod(callback)
+        else:
+            callbackref = weakref.ref(callback)
+        
+        # return the existing callback or create one
+        try:
+            return self._callbacks[page][callbackref]
+        except KeyError:
+            pageref = weakref.ref(page)
+            def newcallback(p):
+                """Called when a subpage (base or overlay) of page has been rendered."""
+                page = pageref()
+                callback = callbackref()
+                if page is not None and callback is not None:
+                    callback(page)
+            self._callbacks.setdefault(page, {})[callbackref] = newcallback
+            return newcallback
+        
     def paint(self, page, painter, rect, callback=None):
-        props = page.x, page.y, page.height, page.width, page.computedRotation
-        for layer, p in enumerate(page.pages):
-            p.x, p.y, p.height, p.width, p.computedRotation = props
+        """Paint the sub pages on pixmaps, and then combine them."""
+        # make the call back return with the original page, not the overlay page
+        newcallback = self.makecallback(callback, page) if callback else None
+        # position the subpages correctly
+        page.fitpages()
+        
+        # get the device pixel ratio to paint for
+        try:
+            ratio = painter.device().devicePixelRatioF()
+        except AttributeError:
+            ratio = painter.device().devicePixelRatio()
+        
+        # let page draw on a pixmap
+        pixmap = QPixmap(rect.size() * ratio)
+        pixmap.setDevicePixelRatio(ratio)
+        pt = QPainter(pixmap)
+        pt.translate(-rect.topLeft())
+        page.base.paint(pt, rect, newcallback)
+        pt.end()
+        
+        # first paint the base page
+        painter.setOpacity(1)
+        painter.drawPixmap(rect.topLeft(), pixmap)
+        
+        # paint the overlay pages on top
+        for layer, p in enumerate(page.overlay):
+            # draw on a pixmap
+            overlayrect = (rect & p.geometry()).translated(-p.pos())
+            pixmap = QPixmap(overlayrect.size() * ratio)
+            pixmap.setDevicePixelRatio(ratio)
+            pt = QPainter(pixmap)
+            pt.translate(-overlayrect.topLeft())
+            p.paint(pt, overlayrect, newcallback)
+            pt.end()
+
+            # paint the overlay page
+            painter.save()
+            painter.translate(p.pos())
             painter.setOpacity(self.opacity[layer])
-            p.paint(painter, rect, callback)
+            painter.drawPixmap(overlayrect.topLeft(), pixmap)
+            painter.restore()
+
+    def unschedule(self, pages, callback):
+        """Reimplemented to unschedule base and overlay pages."""
+        def unschedule_page(page, cb):
+            if page.renderer:
+                page.renderer.unschedule((page,), cb)
+        for page in pages:
+            newcallback = self.makecallback(callback, page) if callback else None
+            unschedule_page(page.base, newcallback)
+            for p in page.overlay:
+                unschedule_page(p, newcallback)
+
+
+
+# install a default renderer, so CompositePage can be used directly
+CompositePage.renderer = CompositeRenderer()
+
 
 
