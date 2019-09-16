7dd12574dd0ddfc756c69d53c7fae240f9512711
Wilbert Berendsen <wbsoft@xs4all.nl>
2019-09-16 16:18:11 +0200

    use an image container, shared among copies of pages. Works well.
    TODO: only load requested size(s)

--- frescobaldi_app/qpageview/image.py
+++ frescobaldi_app/qpageview/image.py
@@ -35,6 +35,7 @@ from PyQt5.QtGui import QImage, QImageReader, QPainter, QTransform
 from .locking import lock
 from . import page
 from . import util
+from . import multipage
 
 
 _pool = weakref.WeakSet()
@@ -43,21 +44,82 @@ _cleanupjob = None
 maxsize = 209715200
 
 
+
+class ImageContainer:
+    """Represent an image, is shared among copies of the "same" Page."""
+    def __init__(self, source, image, autoTransform):
+        self._source = source
+        self._image = image
+        self._autoTransform = autoTransform
+        self._imageJob = None
+        self._thumbnail = None
+        self._thumbnailJob = None
+        self._time = 0
+        if image is not None:
+            manage(self)
+        
+    def _loadImage(self):
+        """Internal. Construct an image reader and return the image."""
+        with lock(self):
+            reader = QImageReader(self._source)
+            reader.setAutoTransform(self._autoTransform)
+            return reader.read()
+
+    def _materialize(self):
+        """Internal. If needed, load the image and deletes the image reader."""
+        if self._image is None:
+            self._image = self._loadImage()
+    
+    def _materializeInBackground(self, page, callback):
+        job = self._imageJob
+        if job is None:
+            from . import backgroundjob
+            job = self._imageJob = backgroundjob.Job()
+            job.work = self._loadImage
+            job.callbacks = callbacks = set()
+            def finalize(result):
+                self._image = result
+                self._imageJob = None
+                for cb in callbacks:
+                    callback(None)
+                manage(self)
+            job.finalize = finalize
+            job.start()
+        if callback:
+            callback = multipage.CallBack(callback, page)
+            job.callbacks.add(callback)
+    
+    def _createThumbnail(self):
+        return self._image.scaled(QSize(200, 200), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+
+    def _thumbnailInBackground(self):
+        job = self._thumbnailJob
+        if job is None:
+            from . import backgroundjob            
+            job = self._thumbnailJob = backgroundjob.Job()
+            job.work = self._createThumbnail
+            def finalize(result):
+                self._thumbnail = result
+                self._thumbnailJob = None
+                self._image = None
+            job.finalize = finalize
+            job.start()
+
+    def image(self):
+        """Return the image if present."""
+        self._time = time.time()
+        return self._image or self._thumbnail
+
+
 class ImagePage(page.AbstractPage):
     """A Page that displays an image in any file format supported by Qt."""
     autoTransform = True    # whether to automatically apply exif transformations
     dpi = 96   # TODO: maybe this can be image dependent.
-    downScaledSize = 200
     
     def __init__(self, filename, size, image=None):
         super().__init__()
-        self.source = filename
+        self._ic = ImageContainer(filename, image, self.autoTransform)
         self.setPageSize(size)
-        self._time = 0
-        self._image = image
-        self._imageJob = None
-        self._imageDownScaled = None
-        self._downScaleJob = None
 
     @classmethod
     def load(cls, filename, renderer=None):
@@ -83,97 +145,41 @@ class ImagePage(page.AbstractPage):
                     size = image.size()
             yield cls(filename, size, image)
 
-    def _loadImage(self):
-        """Internal. Construct an image reader and return the image."""
-        with lock(self):
-            reader = QImageReader(self.source)
-            reader.setAutoTransform(self.autoTransform)
-            return reader.read()
-
-    def _materialize(self):
-        """Internal. If needed, load the image and deletes the image reader."""
-        if self._image is None:
-            self._image = self._loadImage()
-            manage(self)
-
-    def _materializeInBackground(self, callback):
-        """Internal. Load the image in the background without blocking the main thread."""
-        job = self._imageJob
-        if job is None:
-            from . import backgroundjob
-            job = self._imageJob = backgroundjob.Job()
-            job.work = self._loadImage
-            job.callbacks = callbacks = set()
-            def finalize(result):
-                self._image = result
-                self._imageJob = None
-                self._imageDownScaled = None
-                for cb in callbacks:
-                    callback(self)
-                manage(self)
-            job.finalize = finalize
-            job.start()
-        if callback:
-            job.callbacks.add(callback)
-
-    def _downScaleImage(self):
-        """Return a downscaled image."""
-        return self._image.scaled(
-                QSize(self.downScaledSize, self.downScaledSize),
-                Qt.KeepAspectRatio, Qt.SmoothTransformation)
-
-    def _downScaleInBackground(self):
-        job = self._downScaleJob
-        if job is None:
-            from . import backgroundjob
-            job = self._downScaleJob = backgroundjob.Job()
-            job.work = self._downScaleImage
-            job.callbacks = callbacks = set()
-            def finalize(result):
-                self._imageDownScaled = result
-                self._downScaleJob = None
-                self._image = None
-            job.finalize = finalize
-            job.start()
-            
     def paint(self, painter, rect, callback=None):
         """Paint our image in the View."""
-        self._time = time.time()        # keep track of time when last painted
-        if self._image is None:
-            image = self._imageDownScaled
-            if not image or image.width() < (self.height if self.computedRotation & 1 else self.width):
-                self._materializeInBackground(callback)
-            if not image:
+        image = self._ic.image()
+        if image is not None:
+            if image.isNull():
+                # image couldn't be loaded
                 painter.fillRect(rect, self.paperColor or Qt.white)
+                # TODO: paint some icon indicating load failure
                 return
-        elif self._image.isNull():
-            # image couldn't be loaded
-            painter.fillRect(rect, self.paperColor or Qt.white)
-            # TODO: paint some icon indicating load failure
-            return
+            elif image.width() < (self.height if self.computedRotation & 1 else self.width):
+                self._ic._materializeInBackground(self, callback)
         else:
-            image = self._image
+            self._ic._materializeInBackground(self, callback)
+            painter.fillRect(rect, self.paperColor or Qt.white)
+            return
         w, h = image.width(), image.height()
         source = self.mapFromPage(w, h).rect(rect)
         painter.setTransform(self.transform(w, h), True)
-        painter.setRenderHint(QPainter.SmoothPixmapTransform)
+        painter.setRenderHint(QPainter.SmoothPixmapTransform, True)
         painter.drawImage(source, image, source)
 
     def print(self, painter, rect=None, paperColor=None):
         """Paint a page for printing."""
-        self._materialize()
-        if rect is None:
-            image = self._image
-        else:
+        self._ic._materialize()
+        image = self._ic.image()
+        if rect is not None:
             rect = rect.normalized() & self.pageRect()
             # we copy the image, because QSvgGenerator otherwise includes the
             # full image in the resulting SVG file!
-            image = self._image.copy(rect.toRect())
+            image = image.copy(rect.toRect())
         painter.drawImage(QPoint(0, 0), image)
 
     def image(self, rect=None, dpiX=None, dpiY=None, paperColor=None):
         """Return a QImage of the specified rectangle."""
-        self._materialize()
+        self._ic._materialize()
         if rect is None:
             rect = self.rect()
         else:
@@ -192,27 +198,27 @@ class ImagePage(page.AbstractPage):
         m.scale(1 / self.pageWidth, 1 / self.pageHeight)
 
         source = self.transform().inverted()[0].mapRect(rect)
-        return self._image.copy(source).transformed(m, Qt.SmoothTransformation)
+        return self._ic._image.copy(source).transformed(m, Qt.SmoothTransformation)
 
 
-def manage(page):
+def manage(ic):
     """Keep track of memory usage of all pages."""
     global _cleanupjob, _pool, _currentsize, maxsize
-    _pool.add(page)
-    _currentsize += page._image.byteCount()
+    _pool.add(ic)
+    _currentsize += ic._image.byteCount()
     if _currentsize < maxsize:
         return
     _currentsize = 0
     
     # newest first
-    pages = iter(sorted(_pool, key = lambda page: page._time, reverse=True))
-    for page in pages:
-        _currentsize += page._image.byteCount()
+    ics = iter(sorted(_pool, key = lambda ic: ic._time, reverse=True))
+    for ic in ics:
+        _currentsize += ic._image.byteCount()
         if _currentsize > maxsize:
             break
-    pages = list(pages)
-    _pool.difference_update(pages)
-    for page in pages:
-        page._downScaleInBackground()
+    ics = list(ics)
+    _pool.difference_update(ics)
+    for ic in ics:
+        ic._thumbnailInBackground()
 
 
