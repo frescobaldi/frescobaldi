0c502e84b384745e07b2ec20209efc6f19dfbda5
Wilbert Berendsen <wbsoft@xs4all.nl>
2019-09-26 15:34:40 +0200

    add magnifier

--- frescobaldi_app/musicview/widget.py
+++ frescobaldi_app/musicview/widget.py
@@ -77,6 +77,7 @@ class MusicView(QWidget):
 
         self.view = pagedview.View(self)
         self.view.setRubberband(qpageview.rubberband.Rubberband())
+        self.view.setMagnifier(pagedview.Magnifier())
         layout.addWidget(self.view)
         app.settingsChanged.connect(self.readSettings)
         self.readSettings()
