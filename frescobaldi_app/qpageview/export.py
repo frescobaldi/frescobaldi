8f06fb5eec26cc364b0eeef7d12cc7d78e8e6f1b
Wilbert Berendsen <wbsoft@xs4all.nl>
2019-11-11 11:44:19 +0100

    add export.Exporter and subclasses
    to export a rectangular area on any Page to PDF, SVG, EPS or an image.

--- frescobaldi_app/qpageview/export.py
+++ frescobaldi_app/qpageview/export.py
@@ -21,27 +21,280 @@
 Export Pages to different file formats.
 """
 
-from PyQt5.QtCore import QIODevice, QSizeF
-from PyQt5.QtGui import QPageSize, QPdfWriter
+import os
+
+from PyQt5.QtCore import QBuffer, QIODevice, QMimeData, QSizeF, Qt, QUrl
+from PyQt5.QtGui import QGuiApplication, QPageSize, QPdfWriter
+
+from . import poppler
+from . import util
+
+
+class Exporter:
+    """Base class to export a rectangular area of a Page to a file.
+
+    Specialized subclasses implement each format.
+
+    You instantiate a subclass with a Page and a rectangle. The rectangle may
+    be None, to specify the full page. After instantiation, you can set
+    attributes to configure the export. The following attributes are supported:
+
+    resolution = 300
+    autocrop = False
+    oversample = 1
+    grayscale = False
+    paperColor = None
+    forceVector = True  # force the render backend to be Arthur for exporting
+                        # PDF pages to vector-based formats
+
+    After setting the attributes, you call one or more of save(), copyData(),
+    copyFile(), mimeData() or tempFileMimeData(), which will trigger the export
+    because they internally call data(), which caches its return value until
+    setPage() is called again.
+
+    Not all exporters support all attributes, the supportXXX attributes specify
+    whether an attribute is supported or not.
+
+    """
+    # user settings:
+    resolution = 300
+    autocrop = False
+    oversample = 1
+    grayscale = False
+    paperColor = None
+    forceVector = True  # force the render backend to be Arthur for PDF pages
+
+    # properties of exporter:
+    wantsVector = True
+    supportsResolution = True
+    supportsAutocrop = True
+    supportsOversample = True
+    supportsGrayscale = True
+    supportsPaperColor = True
+
+    mimeType = "application/octet-stream"
+    filename = ""
+    defaultBasename = "document"
+    defaultExt = ""
+
+    def __init__(self, page, rect=None):
+        self.setPage(page, rect)
+
+    def setPage(self, page, rect=None):
+        self._page = page.copy()
+        self._rect = rect
+        self._result = None   # where the exported object is stored
+        self._tempFile = None
+
+        if self.forceVector and self.wantsVector and isinstance(page, poppler.PopplerPage):
+            self._page.renderer = page.renderer.copy()
+            self._page.renderer.printRenderBackend = popplerqt5.Poppler.Document.ArthurBackend
+
+    def autoCroppedRect(self):
+        """Return the rect, autocropped if desired."""
+        if not self.autocrop:
+            return self._rect
+        p = self._page
+        dpiX = p.width / p.defaultSize().width() * p.dpi
+        dpiY = p.height / p.defaultSize().height() * p.dpi
+        rect = util.autoCropRect(p.image(self._rect, dpiX, dpiY))
+        if self._rect is not None:
+            rect.translate(self._rect.topLeft())
+        return rect
+
+    def export(self):
+        """Perform the export, based on the settings, and return the exported data object."""
+
+    def successful(self):
+        """Return True when export was succesful."""
+        return self.data() is not None
+
+    def data(self):
+        """Return the export result, assuming it is binary data of the exported file."""
+        if self._result is None:
+            self._result = self.export()
+        return self._result
+
+    def document(self):
+        """Return a one-page Document to display the image to export."""
+
+    def renderer(self):
+        """Return a renderer for the document(). By default, None is returned."""
+        return None
+
+    def copyData(self):
+        """Copy the QMimeData() to the clipboard."""
+        QGuiApplication.clipboard().setMimeData(self.mimeData())
+
+    def mimeData(self):
+        """Return a QMimeData() object representing the exported data."""
+        data = QMimeData()
+        data.setData(self.mimeType, self.data())
+        return data
+
+    def save(self, filename):
+        """Save the exported image to a file."""
+        with open(filename, "wb") as f:
+            f.write(self.data())
+
+    def tempFilename(self):
+        """Save data() to a tempfile and returns the filename."""
+        if self._tempFile is None:
+            if self.filename:
+                basename = os.path.splitext(os.path.basename(self.filename))[0]
+            else:
+                basename = self.defaultBasename
+            d = util.tempdir()
+            fname = self._tempFile = os.path.join(d, basename + self.defaultExt)
+            self.save(fname)
+        return self._tempFile
+
+    def tempFileMimeData(self):
+        """Save the exported image to a temp file and return a QMimeData object for the url."""
+        data = QMimeData()
+        data.setUrls([QUrl.fromLocalFile(self.tempFilename())])
+        return data
+
+    def copyFile(self):
+        """Save the exported image to a temp file and copy its name to the clipboard."""
+        QGuiApplication.clipboard().setMimeData(self.tempFileMimeData())
+
+
+class ImageExporter(Exporter):
+    """Export a rectangular area of a Page (or the whole page) to an image."""
+    wantsVector = False
+    defaultBasename = "image"
+    defaultExt = ".png"
+
+    def export(self):
+        """Create the QImage representing the exported image."""
+        res = self.resolution
+        if self.oversample != 1:
+            res *= self.oversample
+        i = self._page.image(self._rect, res, res, self.paperColor)
+        if self.oversample != 1:
+            i = i.scaled(i.size() / self.oversample, transformMode=Qt.SmoothTransformation)
+        if self.grayscale:
+            i = i.convertToFormat(QImage.Format_Grayscale8)
+        if self.autocrop:
+            i = i.copy(util.autoCropRect(i))
+        return i
+
+    def image(self):
+        return self.data()
+
+    def document(self):
+        from . import image
+        return image.ImageDocument([self.image()], self.renderer())
+
+    def copyData(self):
+        QGuiApplication.clipboard().setImage(self.image())
+
+    def mimeData(self):
+        data = QMimeData()
+        data.setImageData(self.image())
+        return data
+
+    def save(self, filename):
+        self.image().save(filename)
+
+
+class SvgExporter(Exporter):
+    """Export a rectangular area of a Page (or the whole page) to a SVG file."""
+    mimeType = "image/svg"
+    supportsGrayscale = False
+    supportsOversample = False
+    defaultBasename = "image"
+    defaultExt = ".svg"
+
+    def export(self):
+        rect = self.autoCroppedRect()
+        buf = QBuffer()
+        buf.open(QBuffer.WriteOnly)
+        success = self._page.svg(buf, rect, self.resolution, self.paperColor)
+        buf.close()
+        if success:
+            return buf.data()
+
+    def document(self):
+        from . import svg
+        buf = QBuffer(self.data())
+        buf.open(QBuffer.ReadOnly)
+        doc = svg.SvgDocument([buf], self.renderer())
+        buf.close()
+        return doc
+
+
+class PdfExporter(Exporter):
+    """Export a rectangular area of a Page (or the whole page) to a PDF file."""
+    mimeType = "application/pdf"
+    supportsGrayscale = False
+    supportsOversample = False
+    defaultExt = ".pdf"
+
+    def export(self):
+        rect = self.autoCroppedRect()
+        buf = QBuffer()
+        buf.open(QBuffer.WriteOnly)
+        success = self._page.pdf(buf, rect, self.resolution, self.paperColor)
+        buf.close()
+        if success:
+            return buf.data()
+
+    def document(self):
+        from . import poppler
+        buf = QBuffer(self.data())
+        buf.open(QBuffer.ReadOnly)
+        doc = poppler.PopplerDocument(buf, self.renderer())
+        buf.close()
+        return doc
+
+
+class EpsExporter(Exporter):
+    """Export a rectangular area of a Page (or the whole page) to an EPS file."""
+    mimeType = "application/postscript"
+    supportsGrayscale = False
+    supportsOversample = False
+    defaultExt = ".eps"
+
+    def export(self):
+        rect = self.autoCroppedRect()
+        buf = QBuffer()
+        buf.open(QBuffer.WriteOnly)
+        success = self._page.eps(buf, rect, self.resolution, self.paperColor)
+        buf.close()
+        if success:
+            return buf.data()
+
+    def document(self):
+        from . import poppler
+        buf = QBuffer()
+        buf.open(QBuffer.WriteOnly)
+        success = self._page.pdf(buf, rect, self.resolution, self.paperColor)
+        buf.close()
+        buf.open(QBuffer.ReadOnly)
+        doc = poppler.PopplerDocument(buf, self.renderer())
+        buf.close()
+        return doc
 
 
 def pdf(filename, pageList, resolution=72, paperColor=None):
     """Export the pages in pageList to a PDF document.
-    
+
     filename can be a string or any QIODevice. Normally vector graphics are
     rendered, but in cases where that is not possible, the resolution will
     be used to determine the DPI for the generated rendering.
-    
+
     The computedRotation attribute of the pages is used to determine the
     rotation.
-    
+
     Make copies of the pages if you run this function in a background thread.
-    
+
     """
     pdf = QPdfWriter(filename)
     pdf.setCreator("qpageview")
     pdf.setResolution(resolution)
-    
+
     for n, page in enumerate(pageList):
         # map to the original page
         source = page.pageRect()
