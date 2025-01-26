dfc98d64b2d7b7c5dfc6b90a5cc95433d5f51ba9
Federico Bruni <fede@inventati.org>
2025-01-26 15:10:12 +0100

    Rename frescobaldi_app under i18n/ directory
    Fixes #1863

--- i18n/mo-gen.py
+++ i18n/mo-gen.py
@@ -5,7 +5,7 @@ from molint import molint
 
 def compile_mo(path, domain):
     lang = path.stem
-    lang_dir = Path(f"frescobaldi_app/i18n/{lang}/LC_MESSAGES")
+    lang_dir = Path(f"frescobaldi/i18n/{lang}/LC_MESSAGES")
     lang_dir.mkdir(parents=True, exist_ok=True)
     out_file = lang_dir / f"{domain}.mo"
     subprocess.run(["msgfmt", "-o", out_file, path])
