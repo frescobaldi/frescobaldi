1c1389fa0eeccbfd49b1e819b8ea7a2f451afe8c
inkandpaper-app <inkandpaper.app@gmail.com>
2025-08-10 19:16:22 +0200

    Icon theme dark/light for MacOS and Windows
    I've created an icon theme based on Google Material Icons (with some modifications). I also modified the code so that the app recognizes when switching from light to dark themes. The theme is activated by checking the 'system_icons' checkbox in the preferences (if unchecked, the app's icon theme will remain the traditional TangoExt). Changing icons in the preferences causes some minor and pre-existing issues (some icons don't update, or, more rarely, the app quits). Once the preference is set, the app is stable.
    This change should work on Windows (>= 10) and MacOS (but it was tested on MacOS only). It has no effect on Linux platforms:  in this case, unlike the other two, the 'system_icons' preference makes sense, and to implement the new theme I would have had to modify the preferences mechanism more invasively (which my limited knowledge of the app prevents me from doing). However, it shouldn't be difficult for a more experienced programmer to add the theme on Linux as well.
    Fix #1945

--- frescobaldi/icons/__init__.py
+++ frescobaldi/icons/__init__.py
@@ -24,8 +24,10 @@ Icons.
 
 import os
 
-from PyQt6.QtCore import QDir, QFile, QFileInfo, QSettings, QSize
-from PyQt6.QtGui import QIcon
+import platform
+
+from PyQt6.QtCore import QDir, QFile, QFileInfo, QSettings, QSize, Qt
+from PyQt6.QtGui import QIcon, QGuiApplication
 from PyQt6.QtWidgets import QFileIconProvider
 
 import app
@@ -88,7 +90,16 @@ def update_theme():
     """Change the theme for icon lookup after change of style preference"""
     s = QSettings()
     if s.value("system_icons", True, bool):
-        QIcon.setThemeName(s.value("guistyle", "", str))
+        match platform.system():
+            case "Windows" | "Darwin":
+                if QGuiApplication.styleHints().colorScheme() == Qt.ColorScheme.Dark:
+                    QIcon.setThemeName("Dark")
+                elif QGuiApplication.styleHints().colorScheme() == Qt.ColorScheme.Light:
+                    QIcon.setThemeName("Light")
+                else:
+                    QIcon.setThemeName("TangoExt")
+            case _:
+                QIcon.setThemeName(s.value("guistyle", "", str))
     else:
         QIcon.setThemeName("TangoExt") # Our icon theme
 
